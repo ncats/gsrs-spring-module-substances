de9dcb3977202a3d4e43443a96707f464659ed4d
Daniel Katzel <daniel.katzel@nh.gov>
2021-05-26 21:14:05 -0400

    renderer with search result highlights using search context id

---
+++ gsrs-module-substances-core/src/main/java/gsrs/module/substance/repository/ChemicalSubstanceRepository.java
@@ -0,0 +1,15 @@
+package gsrs.module.substance.repository;
+
+import gsrs.repository.GsrsVersionedRepository;
+import ix.ginas.models.v1.ChemicalSubstance;
+import ix.ginas.models.v1.MixtureSubstance;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface ChemicalSubstanceRepository extends GsrsVersionedRepository<ChemicalSubstance, UUID> {
+
+    ChemicalSubstance findByStructure_Id(UUID structureId);
+}
