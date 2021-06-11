ff28e48c3162176870215220279af69ffd7d96b4
Daniel Katzel <daniel.katzel@nh.gov>
2021-06-11 14:30:57 -0400

    use new index events in backups

--- gsrs-module-substances-core/src/main/java/gsrs/module/substance/services/ReindexFromBackups.java
+++ gsrs-module-substances-core/src/main/java/gsrs/module/substance/services/ReindexFromBackups.java
@@ -1,5 +1,7 @@
 package gsrs.module.substance.services;
 
+import gsrs.events.BeginReindexEvent;
+import gsrs.events.DoNotReindexEntityEvent;
 import gsrs.events.MaintenanceModeEvent;
 import gsrs.events.ReindexEntityEvent;
 import gsrs.indexer.IndexCreateEntityEvent;
@@ -15,9 +17,7 @@ import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.IOException;
-import java.util.Collections;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Stream;
@@ -47,7 +47,8 @@ public class ReindexFromBackups implements ReindexService{
         Set<String> seen = Collections.newSetFromMap(new ConcurrentHashMap<>(count));
         String messageTail = " of " + count;
         //single thread for now...
-        eventPublisher.publishEvent(new MaintenanceModeEvent(MaintenanceModeEvent.Mode.BEGIN));
+        UUID reindexId = UUID.randomUUID();
+        eventPublisher.publishEvent(new BeginReindexEvent(reindexId, count));
         try(Stream<BackupEntity> stream = backupRepository.findAll().stream()){
             AtomicLong counter = new AtomicLong();
             stream.forEach(be ->{
@@ -57,6 +58,7 @@ public class ReindexFromBackups implements ReindexService{
                     wrapper.traverse().execute((p, child)->{
                         EntityUtils.EntityWrapper<EntityUtils.EntityWrapper> wrapped = EntityUtils.EntityWrapper.of(child);
                         if(wrapped.isEntity()) {
+
                             //this should speed up indexing so that we only index
                             //things that are roots.  the actual indexing process of the root should handle any
                             //child objects of that root.
@@ -68,13 +70,17 @@ public class ReindexFromBackups implements ReindexService{
                                     //TODO add only index if it has a controller
                                     if (seen.add(keyString)) {
                                         //is this a good idea ?
-                                        ReindexEntityEvent event = new ReindexEntityEvent(key);
+                                        ReindexEntityEvent event = new ReindexEntityEvent(reindexId, key);
                                         eventPublisher.publishEvent(event);
                                     }
                                 } catch (Throwable t) {
                                     System.err.println("error handling " + wrapped);
                                     t.printStackTrace();
                                 }
+                            }else{
+                                //still need to publish something about records we skip since others are keeping count
+
+                                eventPublisher.publishEvent(new DoNotReindexEntityEvent(reindexId, null));
                             }
                         }
 
@@ -85,7 +91,9 @@ public class ReindexFromBackups implements ReindexService{
                 }
                     });
         }
-        eventPublisher.publishEvent(new MaintenanceModeEvent(MaintenanceModeEvent.Mode.END));
+        //other index listeners now figure out when indexing end is so don't need to that publish anymore (here)
+
+
         /*
         new ProcessExecutionService(1, 10).buildProcess(BackupEntity.class)
                 .before(()->{
