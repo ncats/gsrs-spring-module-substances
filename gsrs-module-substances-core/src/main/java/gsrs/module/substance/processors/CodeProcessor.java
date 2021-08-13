36a643924330c8cb7f7b1f8cd9e3c90781c41d25
Tyler Peryea <tylerperyea@gmail.com>
2021-08-13 16:22:33 -0400

    first-pass attempt at codeprocessor port

--- gsrs-module-substances-core/src/main/java/gsrs/module/substance/processors/CodeProcessor.java
+++ gsrs-module-substances-core/src/main/java/gsrs/module/substance/processors/CodeProcessor.java
@@ -2,9 +2,7 @@ package gsrs.module.substance.processors;
 
 import ix.core.EntityProcessor;
 import ix.core.controllers.EntityFactory;
-//import ix.core.util.ConfigHelper;
 import ix.core.util.EntityUtils;
-//import ix.core.util.IOUtil;
 import ix.ginas.models.v1.Code;
 import java.util.Map;
 
@@ -22,33 +20,42 @@ import org.springframework.context.annotation.Configuration;
 public class CodeProcessor implements EntityProcessor<Code> {
 
 	public CodeSystemUrlGenerator codeSystemData;
-
-    @Value("${ix.codeSystemUrlGenerator.class}")
-    private String className;
-
-    @Value("${ix.codeSystemUrlGenerator.json}")
-    private String json;
-    
-    @Autowired
-    private ApplicationContext applicationContext; 
-    
-    private String key = "class";
+	
 	public CodeProcessor(Map with){
 		try{
-            
-            if( className==null && with !=null && with.get(key) !=null) {
-                className= (String) with.get(key);
-            }
-			if(className ==null){
-				log.error("no value for class");
-				throw new IllegalStateException("could not find " + key + " in config file");
-			}
-			Class<?> cls = applicationContext.getClassLoader().loadClass(className);
-            EntityUtils utils = new EntityUtils();
-            codeSystemData = (CodeSystemUrlGenerator) EntityFactory.EntityMapper.FULL_ENTITY_MAPPER().convertValue(json, cls);
-            
+			// It used to be that the codeSystem.json file was found by
+			// looking for the root config JSON found under ix.codeSystemUrlGenerator
+			// Which had both a "class" specified (for the kind of CodeSystemUrlGenerator
+			// to use) and a "json" property which contained the raw JSON to deserialize
+			// the CodeSystemUrlGenerator with. This gave some flexibility (perhaps too much flexibility)
+			// to how a CodeSystemUrlGenerator could be made. In practice it was always instantiating
+			// a DefaultCodeSystemUrlGenerator which really just used a filename.
+			
+			//String key = "ix.codeSystemUrlGenerator.class";
+			//String classname = Play.application().configuration().getString(key);
+			//if(classname ==null){
+			//	System.out.println("config =\n" + Play.application().configuration().asMap());
+			//	throw new IllegalStateException("could not find " + key + " in config file");
+			//}
+			//Class<?> cls = IOUtil.getGinasClassLoader().loadClass(classname);
 			//codeSystemData = (CodeSystemUrlGenerator) ConfigHelper.readFromJson("ix.codeSystemUrlGenerator.json", cls);
 
+			//Now, instead, just read these properties from the "with" part of then entity processor initializer.
+			//It may still be a bit too abstract for practical purposes, but it's not too bad
+			
+			String key = "class";
+			String classname = (String)with.get(key);
+			if(classname ==null){
+				System.out.println("config =\n" + with);
+				throw new IllegalStateException("could not find " + key + " in codesystem initialization file");
+			}
+			Class<?> cls = CodeProcessor.class.getClassLoader().loadClass(classname);
+			if(with.get("json")!=null){
+				Object jsonObj=with.get("json");
+				ObjectMapper mapper = new ObjectMapper();
+				//convert from the 
+        			codeSystemData = (CodeSystemUrlGenerator) mapper.convertValue(jsonObj, cls);
+			}
 		}catch(Exception e){
 			e.printStackTrace();
 			throw new RuntimeException(e);
@@ -74,43 +81,8 @@ public class CodeProcessor implements EntityProcessor<Code> {
 	}
 
 	@Override
-	public void postPersist(Code obj) throws ix.core.EntityProcessor.FailProcessingException {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void preRemove(Code obj) throws ix.core.EntityProcessor.FailProcessingException {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void postRemove(Code obj) throws ix.core.EntityProcessor.FailProcessingException {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
 	public void preUpdate(Code obj) throws ix.core.EntityProcessor.FailProcessingException {
 		generateURL(obj);
 	}
-
-	@Override
-	public void postUpdate(Code obj) throws ix.core.EntityProcessor.FailProcessingException {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void postLoad(Code obj) throws ix.core.EntityProcessor.FailProcessingException {
-		// TODO Auto-generated method stub
-		
-	}
-
-    @Override
-    public Class<Code> getEntityClass() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
 
 }
