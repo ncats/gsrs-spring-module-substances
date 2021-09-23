60864ee9778f3cdfc1006ad395ab0046c08822d4
Daniel Katzel <daniel.katzel@nh.gov>
2021-09-23 11:32:17 -0400

    adding back the special characters in tomcat configuration as a Bean

--- gsrs-module-substance-example/src/main/java/example/GsrsModuleSubstanceApplication.java
+++ gsrs-module-substance-example/src/main/java/example/GsrsModuleSubstanceApplication.java
@@ -52,22 +52,22 @@ public class GsrsModuleSubstanceApplication {
         };
     }
 
-//    @Bean
-//    public WebServerFactoryCustomizer<TomcatServletWebServerFactory>
-//    containerCustomizer(){
-//        return new EmbeddedTomcatCustomizer();
-//    }
-//
-//    private static class EmbeddedTomcatCustomizer implements WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
-//
-//        @Override
-//        public void customize(TomcatServletWebServerFactory factory) {
-//            factory.addConnectorCustomizers((TomcatConnectorCustomizer) connector -> {
-//                connector.setAttribute("relaxedPathChars", "<>[\\]^`{|}");
-//                connector.setAttribute("relaxedQueryChars", "<>[\\]^`{|}");
-//            });
-//        }
-//    }
+    @Bean
+    public WebServerFactoryCustomizer<TomcatServletWebServerFactory>
+    containerCustomizer(){
+        return new EmbeddedTomcatCustomizer();
+    }
+
+    private static class EmbeddedTomcatCustomizer implements WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
+
+        @Override
+        public void customize(TomcatServletWebServerFactory factory) {
+            factory.addConnectorCustomizers((TomcatConnectorCustomizer) connector -> {
+                connector.setAttribute("relaxedPathChars", "<>[\\]^`{|}");
+                connector.setAttribute("relaxedQueryChars", "<>[\\]^`{|}");
+            });
+        }
+    }
 
     @Bean
     public DeprecatedIndexValueMaker deprecatedIndexValueMaker(){
