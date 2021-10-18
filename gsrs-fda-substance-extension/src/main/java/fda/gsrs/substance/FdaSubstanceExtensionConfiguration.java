898e1be47051221e3fd2e37b69afd49097ce26d1
Daniel Katzel <daniel.katzel@nh.gov>
2021-10-18 17:23:34 -0400

    merged latest  changes

--- gsrs-fda-substance-extension/src/main/java/fda/gsrs/substance/FdaSubstanceExtensionConfiguration.java
+++ gsrs-fda-substance-extension/src/main/java/fda/gsrs/substance/FdaSubstanceExtensionConfiguration.java
@@ -1,10 +1,8 @@
 package fda.gsrs.substance;
 
-import gov.hhs.gsrs.applications.api.*;
-import gov.hhs.gsrs.products.api.*;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import gov.hhs.gsrs.applications.api.ApplicationsApi;
+import gov.hhs.gsrs.products.api.ProductsApi;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
