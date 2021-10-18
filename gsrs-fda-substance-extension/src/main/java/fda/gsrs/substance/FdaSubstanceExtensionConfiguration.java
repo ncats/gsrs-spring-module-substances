202201fee97075553ae01c19288f59eaed13f4e4
Daniel Katzel <daniel.katzel@nh.gov>
2021-10-18 17:21:01 -0400

    added ProductsAPI

--- gsrs-fda-substance-extension/src/main/java/fda/gsrs/substance/FdaSubstanceExtensionConfiguration.java
+++ gsrs-fda-substance-extension/src/main/java/fda/gsrs/substance/FdaSubstanceExtensionConfiguration.java
@@ -2,6 +2,7 @@ package fda.gsrs.substance;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import gov.hhs.gsrs.applications.api.ApplicationsApi;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,6 +16,11 @@ public class FdaSubstanceExtensionConfiguration {
     //public ApplicationsApi applicationsApi(RestTemplateBuilder builder, @Value("${application.host}") String applicationHost){
         applicationApiConfiguration.configure(builder);
         return new ApplicationsApi(builder,applicationApiConfiguration.getBaseURL(), mapper );
+    }
+
+    @Bean
+    public ProductsApi productsApi(RestTemplateBuilder builder, @Value("${gsrs.microservice.products.api.baseURL}") String productHost){
+        return new ProductsApi(builder,productHost, mapper);
     }
 
     private ObjectMapper mapper = new ObjectMapper();
