0683ff5f9f431edc468cc7bbb1f3ba1b41924a3e
Daniel Katzel <daniel.katzel@nh.gov>
2021-10-18 17:37:23 -0400

    added header support for Products too

--- gsrs-fda-substance-extension/src/main/java/fda/gsrs/substance/FdaSubstanceExtensionConfiguration.java
+++ gsrs-fda-substance-extension/src/main/java/fda/gsrs/substance/FdaSubstanceExtensionConfiguration.java
@@ -3,7 +3,6 @@ package fda.gsrs.substance;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import gov.hhs.gsrs.applications.api.ApplicationsApi;
 import gov.hhs.gsrs.products.api.ProductsApi;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,14 +13,14 @@ public class FdaSubstanceExtensionConfiguration {
     //Put FDA specific API @Bean definitions here
     @Bean
     public ApplicationsApi applicationsApi(RestTemplateBuilder builder, ApplicationApiConfiguration applicationApiConfiguration){
-        //public ApplicationsApi applicationsApi(RestTemplateBuilder builder, @Value("${application.host}") String applicationHost){
-        applicationApiConfiguration.configure(builder);
+       applicationApiConfiguration.configure(builder);
         return new ApplicationsApi(builder,applicationApiConfiguration.getBaseURL(), mapper );
     }
 
     @Bean
-    public ProductsApi productsApi(RestTemplateBuilder builder, @Value("${gsrs.microservice.products.api.baseURL}") String productHost){
-        return new ProductsApi(builder,productHost, mapper);
+    public ProductsApi productsApi(RestTemplateBuilder builder, ProductsApiConfiguration productsApiConfiguration){
+        productsApiConfiguration.configure(builder);
+        return new ProductsApi(builder,productsApiConfiguration.getBaseURL(), mapper);
     }
 
     private ObjectMapper mapper = new ObjectMapper();
--- gsrs-fda-substance-extension/src/main/java/fda/gsrs/substance/ProductsApiConfiguration.java
+++ gsrs-fda-substance-extension/src/main/java/fda/gsrs/substance/ProductsApiConfiguration.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ConfigurationProperties("gsrs.microservice.applications.api")
-public class ApplicationApiConfiguration extends AbstractGsrsRestApiConfiguration{
+public class ProductsApiConfiguration extends AbstractGsrsRestApiConfiguration{
 
 
 }
