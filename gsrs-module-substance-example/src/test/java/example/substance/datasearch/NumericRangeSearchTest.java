2c9cddc4a555f82575ca4ed7377a3988f098d66a
Mitch Miller <mitch.miller@thinkscience.us>
2021-11-03 20:07:52 -0400

    WIP Added 2 unit tests with range search on numeric fields. Neither returns any hits.  Probably a bug somewhere!

---
+++ gsrs-module-substance-example/src/test/java/example/substance/datasearch/NumericRangeSearchTest.java
@@ -0,0 +1,155 @@
+package example.substance.datasearch;
+
+import example.substance.AbstractSubstanceJpaFullStackEntityTest;
+import gsrs.module.substance.controllers.SubstanceLegacySearchService;
+import gsrs.module.substance.indexers.SubstanceDefinitionalHashIndexer;
+import gsrs.module.substance.services.DefinitionalElementFactory;
+import gsrs.springUtils.AutowireHelper;
+import gsrs.startertests.TestGsrsValidatorFactory;
+import gsrs.startertests.TestIndexValueMakerFactory;
+import gsrs.validator.DefaultValidatorConfig;
+import gsrs.validator.ValidatorConfig;
+import ix.core.chem.StructureProcessor;
+import ix.core.search.SearchRequest;
+import ix.core.search.SearchResult;
+import ix.core.util.EntityUtils;
+import ix.ginas.models.v1.ChemicalSubstance;
+import ix.ginas.models.v1.Substance;
+import ix.ginas.utils.validation.validators.ChemicalValidator;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.extern.slf4j.Slf4j;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.transaction.support.TransactionTemplate;
+
+/**
+ *
+ * @author mitch
+ */
+@WithMockUser(username = "admin", roles = "Admin")
+@Slf4j
+public class NumericRangeSearchTest extends AbstractSubstanceJpaFullStackEntityTest {
+
+    public NumericRangeSearchTest() {
+    }
+
+    @Autowired
+    private SubstanceLegacySearchService searchService;
+
+    @Autowired
+    private DefinitionalElementFactory definitionalElementFactory;
+
+    @Autowired
+    private TestIndexValueMakerFactory testIndexValueMakerFactory;
+
+    @Autowired
+    StructureProcessor structureProcessor;
+
+    @Autowired
+    private TestGsrsValidatorFactory factory;
+
+    private String fileName = "rep18.gsrs";
+
+    @BeforeEach
+    public void clearIndexers() throws IOException {
+        SubstanceDefinitionalHashIndexer hashIndexer = new SubstanceDefinitionalHashIndexer();
+        AutowireHelper.getInstance().autowire(hashIndexer);
+        testIndexValueMakerFactory.addIndexValueMaker(hashIndexer);
+        {
+            ValidatorConfig config = new DefaultValidatorConfig();
+            config.setValidatorClass(ChemicalValidator.class);
+            config.setNewObjClass(ChemicalSubstance.class);
+            factory.addValidator("substances", config);
+        }
+
+        File dataFile = new ClassPathResource(fileName).getFile();
+        loadGsrsFile(dataFile);
+    }
+
+    @Test
+    public void testSearchByDateRange() {
+
+        log.trace("starting in testSearchByDateRange");
+        String start = "1620121913352";
+        String end = "1620121913354";
+
+        SearchRequest request = new SearchRequest.Builder()
+                .kind(Substance.class)
+                .fdim(0)
+                .query("root_created:%5B" + start + " TO " + end + "%5D")
+                .top(Integer.MAX_VALUE)
+                .build();
+
+        log.trace("query: " +request.getQuery());
+        List<Substance> substances = getSearchList(request);
+
+        log.trace("substances size: " + substances.size());
+        String actualId = substances.stream()
+                .map(s -> s.uuid.toString())
+                .findFirst().get();
+        log.trace("actualId: " + actualId);
+
+        String expectedId = "ac776f92-b90f-48a0-a54e-7461a60c84b3";
+        assertEquals(expectedId, actualId);
+    }
+
+    @Test
+    public void testSearchByMwtRange() {
+
+        log.trace("starting in testSearchByMwtRange");
+        String start = "354.0";
+        String end = "356.0";
+
+        SearchRequest request = new SearchRequest.Builder()
+                .kind(Substance.class)
+                .fdim(0)
+                .query("root_structure_mwt:[" + start + " TO " + end + "]")
+                .top(Integer.MAX_VALUE)
+                .build();
+
+        log.trace("query: " +request.getQuery());
+        List<Substance> substances = getSearchList(request);
+
+        log.trace("substances size: " + substances.size());
+        String actualId = substances.stream()
+                .map(s -> s.uuid.toString())
+                .findFirst().get();
+        log.trace("actualId: " + actualId);
+
+        String expectedId = "ac776f92-b90f-48a0-a54e-7461a60c84b3";
+        assertEquals(expectedId, actualId);
+    }
+
+    /**
+     * Return a list of substances based on the {@link SearchRequest}. This
+     * takes care of some tricky transaction issues.
+     *
+     * @param sr
+     * @return
+     */
+    private List<Substance> getSearchList(SearchRequest sr) {
+        TransactionTemplate transactionSearch = new TransactionTemplate(transactionManager);
+        List<Substance> substances = transactionSearch.execute(ts -> {
+            try {
+                SearchResult sresult = searchService.search(sr.getQuery(), sr.getOptions());
+                List<Substance> first = sresult.getMatches();
+                return first.stream()
+                        //force fetching
+                        .peek(ss -> EntityUtils.EntityWrapper.of(ss).toInternalJson())
+                        .collect(Collectors.toList());
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+
+            }
+        });
+        return substances;
+    }
+
+}
