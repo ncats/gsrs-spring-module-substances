ea82d48762e6ededccbb6cc85dacb9d675e049f3
Daniel Katzel <daniel.katzel@nh.gov>
2021-11-05 17:06:26 -0400

    updated to latest way to specify rest template for APIs

--- gsrs-fda-substance-extension/src/main/java/fda/gsrs/substance/FdaSubstanceExtensionConfiguration.java
+++ gsrs-fda-substance-extension/src/main/java/fda/gsrs/substance/FdaSubstanceExtensionConfiguration.java
@@ -3,7 +3,6 @@ package fda.gsrs.substance;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import gov.hhs.gsrs.applications.api.ApplicationsApi;
 import gov.hhs.gsrs.products.api.ProductsApi;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,16 +12,13 @@ public class FdaSubstanceExtensionConfiguration {
     //Put FDA specific API @Bean definitions here
     @Bean
     public ApplicationsApi applicationsApi(ApplicationApiConfiguration applicationApiConfiguration){
-        RestTemplateBuilder builder = new RestTemplateBuilder();
-       applicationApiConfiguration.configure(builder);
-        return new ApplicationsApi(builder,applicationApiConfiguration.getBaseURL(), mapper );
+
+        return new ApplicationsApi(applicationApiConfiguration.createNewRestTemplateBuilder(),applicationApiConfiguration.getBaseURL(), mapper );
     }
 
     @Bean
     public ProductsApi productsApi( ProductsApiConfiguration productsApiConfiguration){
-        RestTemplateBuilder builder = new RestTemplateBuilder();
-        productsApiConfiguration.configure(builder);
-        return new ProductsApi(builder,productsApiConfiguration.getBaseURL(), mapper);
+        return new ProductsApi(productsApiConfiguration.createNewRestTemplateBuilder(),productsApiConfiguration.getBaseURL(), mapper);
     }
 
     private ObjectMapper mapper = new ObjectMapper();
