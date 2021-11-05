473881712be582f648860c6112bbf2a8efda6cd3
Tyler Peryea <tylerperyea@gmail.com>
2021-11-05 12:39:03 -0400

    updated config REST to be in starter

--- gsrs-fda-substance-extension/src/main/java/fda/gsrs/substance/ProductsApiConfiguration.java
+++ gsrs-fda-substance-extension/src/main/java/fda/gsrs/substance/ProductsApiConfiguration.java
@@ -3,6 +3,8 @@ package fda.gsrs.substance;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+import gsrs.api.AbstractGsrsRestApiConfiguration;
+
 @Configuration
 @ConfigurationProperties("gsrs.microservice.products.api")
 public class ProductsApiConfiguration extends AbstractGsrsRestApiConfiguration{
