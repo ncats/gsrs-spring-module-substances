f5413cd05958fd0cb498d239040ea51c2e3eb73a
Mitch Miller <mitch.miller@thinkscience.us>
2021-12-20 23:09:10 -0500

    Building some of the components of NSRS within a branch of the main GSRS substance module.

---
+++ gsrs-module-substance-example/src/test/java/example/substance/validation/DEAValidatorTest.java
@@ -0,0 +1,82 @@
+package example.substance.validation;
+
+import example.substance.AbstractSubstanceJpaEntityTest;
+import gsrs.startertests.TestGsrsValidatorFactory;
+import gsrs.validator.DefaultValidatorConfig;
+import gsrs.validator.ValidatorConfig;
+import ix.core.validator.ValidationMessage;
+import ix.core.validator.ValidationResponse;
+import ix.ginas.modelBuilders.ChemicalSubstanceBuilder;
+import ix.ginas.models.v1.ChemicalSubstance;
+import ix.ginas.models.v1.Substance;
+import ix.ginas.utils.validation.validators.DEAValidator;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.stream.Stream;
+
+@Slf4j
+public class DEAValidatorTest extends AbstractSubstanceJpaEntityTest {
+
+    private static boolean configured = false;
+
+    @Autowired
+    private TestGsrsValidatorFactory factory;
+
+    @BeforeEach
+    public void setup() {
+        if (!configured) {
+            ValidatorConfig config = new DefaultValidatorConfig();
+            config.setValidatorClass(DEAValidator.class);
+            config.setNewObjClass(Substance.class);
+            factory.addValidator("substances", config);
+            configured = true;
+            System.out.println("configured!");
+        }
+    }
+
+    @Test
+    public void testInit() throws IOException {
+        DEAValidator validator = new DEAValidator();
+        validator.init();
+
+        Map<String, String> inchiKeyToDeaNumber= validator.getInchiKeyToDeaNumber();
+
+        Assertions.assertTrue(inchiKeyToDeaNumber.size()>0);
+    }
+
+    @Test
+    public void testSubstance1() throws Exception {
+        //create a substance that's on the list
+        ChemicalSubstanceBuilder builder = new ChemicalSubstanceBuilder();
+        ChemicalSubstance chemical= builder.setStructureWithDefaultReference("COc1ccc(CC(C)N)cc1")
+                .addName("d")
+                .addCode("UNII", "OVB8F8P39Q")
+                .build();
+        ValidationResponse response = substanceEntityService.validateEntity(chemical.toFullJsonNode());
+        Stream<ValidationMessage> s1 = response.getValidationMessages().stream();
+        Assertions.assertTrue( s1.anyMatch(m->
+                m.getMessageType()== ValidationMessage.MESSAGE_TYPE.WARNING && m.getMessage().contains("This substance has DEA schedule:")));
+    }
+
+    @Test
+    public void testSubstance2() throws Exception {
+        //create a substance that's on the list
+        ChemicalSubstanceBuilder builder = new ChemicalSubstanceBuilder();
+        ChemicalSubstance chemical= builder.setStructureWithDefaultReference("CCCCCN1N=C(C(=O)NC23CC4CC(CC(C4)C2)C3)C5=C1C=CC=C5")
+                .addName("APINACA and AKB48 N-(1-Adamantyl)-1-pentyl-1Hindazole-3-carboxamide")
+                .addCode("UNII", "MHR0400Y84")
+                .build();
+        ValidationResponse response = substanceEntityService.validateEntity(chemical.toFullJsonNode());
+        Substance sub = (Substance) response.getNewObject();
+        String expectedValue = "DEA-7048";
+        Assertions.assertTrue( sub.codes.stream().anyMatch(c->c.codeSystem.equals(DEAValidator.DEA_NUMBER_CODE_SYSTEM) &&
+                c.code.equals(expectedValue)));
+    }
+
+}
