26011091d0a1d988608190883bc99270753175aa
Mitch Miller <mitch.miller@thinkscience.us>
2022-01-25 22:43:42 -0500

    restored ReindexTaskInitializer

--- gsrs-module-substances-core/src/main/java/gsrs/module/substance/tasks/ReindexTaskInitializer.java
+++ gsrs-module-substances-core/src/main/java/gsrs/module/substance/tasks/ReindexTaskInitializer.java
@@ -1,38 +1,20 @@
 package gsrs.module.substance.tasks;
 
-import gsrs.events.MaintenanceModeEvent;
-import gsrs.indexer.IndexCreateEntityEvent;
 import gsrs.module.substance.services.ReindexService;
-import gsrs.repository.BackupRepository;
 import gsrs.scheduledTasks.ScheduledTaskInitializer;
-
 import gsrs.scheduledTasks.SchedulerPlugin;
-import gsrs.security.GsrsSecurityUtils;
-import gsrs.springUtils.GsrsSpringUtils;
-import ix.core.models.BackupEntity;
-import ix.core.models.Role;
-import ix.core.models.UserProfile;
-
-import ix.core.util.EntityUtils;
-import ix.core.utils.executor.ProcessListener;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
 import java.util.UUID;
-import java.util.concurrent.ConcurrentHashMap;
 
 public class ReindexTaskInitializer extends ScheduledTaskInitializer {
 
     @Autowired
     private ReindexService reindexService;
 
-	@Override
-	public void run(SchedulerPlugin.TaskListener l){
+    @Override
+    public void run(SchedulerPlugin.JobStats stats, SchedulerPlugin.TaskListener l){
         UUID uuid = UUID.randomUUID();
         try {
             reindexService.execute(uuid, l);
@@ -40,12 +22,12 @@ public class ReindexTaskInitializer extends ScheduledTaskInitializer {
             e.printStackTrace();
         }
 
-	}
+    }
 
-	@Override
-	public String getDescription() {		
-		return "Reindex all core entities from backup tables";
-	}
+    @Override
+    public String getDescription() {
+        return "Reindex all core entities from backup tables";
+    }
 
 
 }
