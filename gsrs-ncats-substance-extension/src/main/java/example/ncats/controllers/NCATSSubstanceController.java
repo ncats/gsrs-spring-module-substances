c1ef00110a34e844d0e9f78560ccbbf08ab144de
Mitch Miller <mitch.miller@thinkscience.us>
2022-01-27 19:53:40 -0500

    tweaks to NCATSSubstanceController

--- gsrs-ncats-substance-extension/src/main/java/example/ncats/controllers/NCATSSubstanceController.java
+++ gsrs-ncats-substance-extension/src/main/java/example/ncats/controllers/NCATSSubstanceController.java
@@ -6,8 +6,9 @@ import gsrs.controller.IdHelpers;
 import gsrs.controller.PostGsrsRestApiMapping;
 import gsrs.module.substance.SubstanceEntityServiceImpl;
 import gsrs.module.substance.utils.NCATSFileUtils;
+import ix.ginas.models.v1.Substance;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.MediaType;
+import org.springframework.hateoas.server.ExposesResourceFor;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
@@ -16,27 +17,29 @@ import javax.validation.constraints.NotNull;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.util.Map;
 import java.util.Set;
 
 @Slf4j
-@GsrsRestApiController(context = SubstanceEntityServiceImpl.CONTEXT,  idHelper = IdHelpers.UUID)
+@ExposesResourceFor(Substance.class)
+@GsrsRestApiController(context = SubstanceEntityServiceImpl.CONTEXT)
 public class NCATSSubstanceController {
 
     @PostGsrsRestApiMapping(path="/getFieldsForSDFile")
-    public Object fieldsForSDF(@NotNull @RequestBody String fileData,
+    public Object fieldsForSDF(@NotNull @RequestBody MultipartFile file,
                                           @RequestParam Map<String, String> processingParameters) throws IOException {
         log.trace("starting in fieldsForSDF");
-        /*String fileName = file.getName();*/
-        String fileName ="temp1.sdf";
+        String fileName = file.getName();
         log.debug("using fileName: " + fileName);
-        File tempSdFile= stringToFile( fileData, fileName);
+        File tempSdFile= multipartToFile( file, fileName);
         Set<String> fields =NCATSFileUtils.getSdFileFields(tempSdFile.getPath());
         log.trace("total fields: " + fields.size());
         return fields;
     }
 
+    /*
+    Adding a simple GET as a test
+     */
     @GetGsrsRestApiMapping("/getFieldsForSDFile")
     public String getFieldInfo() {
         return "Structure,ID";
