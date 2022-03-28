a3ad782039f638f8991cc006f7f8fc065a9e2059
Tyler Peryea <tylerperyea@gmail.com>
2022-03-28 14:40:54 -0400

    attempting to change heap size

--- gsrs-module-substance-example/src/test/java/example/loadertests/LegacySubstanceJSONBulkLoadTest.java
+++ gsrs-module-substance-example/src/test/java/example/loadertests/LegacySubstanceJSONBulkLoadTest.java
@@ -47,7 +47,7 @@ public class LegacySubstanceJSONBulkLoadTest extends AbstractSubstanceJpaFullSta
     private GsrsValidatorFactory validatorFactory;
 
     public LegacySubstanceJSONBulkLoadTest(){
-        super(false);
+        super(true);
     }
     @Transactional
     @Test
