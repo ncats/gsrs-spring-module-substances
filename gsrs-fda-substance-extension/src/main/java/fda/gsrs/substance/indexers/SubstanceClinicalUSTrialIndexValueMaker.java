47915ae1fefc3c19a077ba3f72d0362df4e0b009
A. Welsch <alexander.welsch@fda.hhs.gov>
2022-04-08 11:12:41 -0400

    printlns

--- gsrs-fda-substance-extension/src/main/java/fda/gsrs/substance/indexers/SubstanceClinicalUSTrialIndexValueMaker.java
+++ gsrs-fda-substance-extension/src/main/java/fda/gsrs/substance/indexers/SubstanceClinicalUSTrialIndexValueMaker.java
@@ -27,12 +27,17 @@ public class SubstanceClinicalUSTrialIndexValueMaker implements IndexValueMaker<
 
     @Override
     public void createIndexableValues(Substance substance, Consumer<IndexableValue> consumer) {
+
+		System.out.println("I AM IN SIDE SubstanceClinicalUSTrialIndexValueMaker.createIndexableValues 1");
         try{
+
         	SearchRequest searchRequest = SearchRequest.builder().q("entity_link_substances:\"" + substance.uuid + "\"").top(1000000).simpleSearchOnly(true).build();
 			SearchResult<ClinicalTrialUSDTO> searchResult = clinicalTrialUSApi.search(searchRequest);
 			List<ClinicalTrialUSDTO> ctusList = searchResult.getContent();
 			//substances may have more than one ctus and therefore multiple status values
 			ctusList.forEach(ctus -> {
+				System.out.println("I AM IN SIDE SubstanceClinicalUSTrialIndexValueMaker.createIndexableValues 2");
+
 				consumer.accept(IndexableValue.simpleFacetStringValue("Clinical Trial US Number", ctus.getTrialNumber()));
 				consumer.accept(IndexableValue.simpleFacetStringValue("Clinical Trial US Status", ctus.getStatus()));
 			});
