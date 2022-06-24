5cd28a329d9f5bc8ffefe974831bb8d69b631ec8
A. Welsch <alexander.welsch@fda.hhs.gov>
2022-06-24 15:20:12 -0400

    bump ct module dependency to 0.2.3

--- installExtraJars.cmd
+++ installExtraJars.cmd
@@ -1,3 +1,3 @@
 ./mvnw.cmd install:install-file -Dfile=extraJars/applications-api-0.1.3-SNAPSHOT.jar
 ./mvnw.cmd install:install-file -Dfile=extraJars/products-api-0.1.3-SNAPSHOT.jar
-./mvnw.cmd install:install-file -Dfile=extraJars/clinical-trials-api-0.2.2-SNAPSHOT.jar
+./mvnw.cmd install:install-file -Dfile=extraJars/clinical-trials-api-0.2.3-SNAPSHOT.jar
