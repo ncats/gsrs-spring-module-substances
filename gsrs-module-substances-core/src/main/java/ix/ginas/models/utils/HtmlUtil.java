a3843eba6ddeaaace01d6f5e468c59d5678044ee
epuzanov <epuzanov@users.noreply.github.com>
2022-07-26 11:31:57 +0200

    add safe tags check to the TruncateVisitor

--- gsrs-module-substances-core/src/main/java/ix/ginas/models/utils/HtmlUtil.java
+++ gsrs-module-substances-core/src/main/java/ix/ginas/models/utils/HtmlUtil.java
@@ -1,12 +1,8 @@
 package ix.ginas.models.utils;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.nodes.Node;
-import org.jsoup.nodes.TextNode;
+import java.util.Set;
+import org.jsoup.nodes.*;
 import org.jsoup.parser.Parser;
-import org.jsoup.safety.Safelist;
 import org.jsoup.select.NodeTraversor;
 import org.jsoup.select.NodeVisitor;
 
@@ -21,6 +17,7 @@ public final class HtmlUtil {
         private Element dst;
         private Element cur;
         private boolean stop = false;
+        private static final Set<String> safetags = Set.of("b", "i", "small", "sub", "sup");
 
         public TruncateVisitor (Element dst, int maxLen) {
             this.maxLen = maxLen;
@@ -32,40 +29,39 @@ public final class HtmlUtil {
             if (depth > 0) {
                 if (node instanceof Element) {
                     Element curElement = (Element) node;
-                    cur = cur.appendElement(curElement.tagName());
-                    String resHtml = dst.html();
-                    if (resHtml.length() > maxLen) {
-                        cur.remove();
-                        throw new IllegalStateException();
+                    if (safetags.contains(curElement.tagName())) {
+                        cur = cur.appendElement(curElement.tagName());
+                        String resHtml = dst.html();
+                        if (resHtml.length() > maxLen) {
+                            cur.remove();
+                            throw new IllegalStateException();
+                        }
                     }
                 } else if (node instanceof TextNode) {
-                    String curText = ((TextNode) node).getWholeText();
-                    String resHtml = dst.html();
-                    if (curText.length() + resHtml.length() > maxLen) {
-                        cur.appendText(curText.substring(0, maxLen - resHtml.length()));
-                        throw new IllegalStateException();
-                    } else {
-                        cur.appendText(curText);
+                    String parentTag = ((Element)node.parent()).tagName();
+                    if (safetags.contains(parentTag) || (parentTag == "body" && depth == 1)) {
+                        String curText = ((TextNode) node).getWholeText();
+                        String resHtml = dst.html();
+                        if (curText.length() + resHtml.length() > maxLen) {
+                            cur.appendText(curText.substring(0, maxLen - resHtml.length()));
+                            throw new IllegalStateException();
+                        } else {
+                            cur.appendText(curText);
+                        }
                     }
                 }
             }
         }
 
         public void tail(Node node, int depth) {
-            if (depth > 0 && node instanceof Element) {
+            if (depth > 0 && node instanceof Element && safetags.contains(((Element)node).tagName())) {
                 cur = cur.parent();
             }
         }
     }
 
     public static String truncateString(String s, int len){
-        Safelist sl = Safelist.none().addTags("i", "b", "sub", "sup", "small");
-        String cleanText = Jsoup.clean(s, sl);
-        if (s.length() <= len) {
-            return cleanText;
-        }
-
-        Document srcDoc = Parser.parseBodyFragment(cleanText, "");
+        Document srcDoc = Parser.parseBodyFragment(s, "");
         srcDoc.outputSettings().prettyPrint(false);
 
         Document dstDoc = Document.createShell(srcDoc.baseUri());
