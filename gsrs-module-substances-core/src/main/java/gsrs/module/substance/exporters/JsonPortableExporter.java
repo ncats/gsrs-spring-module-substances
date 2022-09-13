ba2d8bd62066d3699f108e74d36ef6283dab1c36
epuzanov <egor.puzanov@bfarm.de>
2022-09-13 15:58:18 +0200

    move Jose crypto functions to the JoseUtil module

--- gsrs-module-substances-core/src/main/java/gsrs/module/substance/exporters/JsonPortableExporter.java
+++ gsrs-module-substances-core/src/main/java/gsrs/module/substance/exporters/JsonPortableExporter.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.fasterxml.jackson.databind.node.TextNode;
 
+import gsrs.module.substance.utils.JoseUtil;
 import ix.core.controllers.EntityFactory;
 import ix.ginas.exporters.Exporter;
 import ix.ginas.models.v1.Substance;
@@ -15,9 +16,6 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.security.PrivateKey;
 import java.time.format.DateTimeFormatter;
 import java.time.ZonedDateTime;
 import java.time.ZoneOffset;
@@ -25,32 +23,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-
-import org.apache.cxf.common.util.StringUtils;
-import org.apache.cxf.rs.security.jose.common.JoseType;
-import org.apache.cxf.rs.security.jose.jwa.ContentAlgorithm;
-import org.apache.cxf.rs.security.jose.jwa.KeyAlgorithm;
-import org.apache.cxf.rs.security.jose.jwa.SignatureAlgorithm;
-import org.apache.cxf.rs.security.jose.jwe.ContentEncryptionProvider;
-import org.apache.cxf.rs.security.jose.jwe.JweEncryption;
-import org.apache.cxf.rs.security.jose.jwe.JweEncryptionProvider;
-import org.apache.cxf.rs.security.jose.jwe.JweHeaders;
-import org.apache.cxf.rs.security.jose.jwe.JweJsonProducer;
-import org.apache.cxf.rs.security.jose.jwe.JweUtils;
-import org.apache.cxf.rs.security.jose.jwe.KeyEncryptionProvider;
-import org.apache.cxf.rs.security.jose.jwk.JsonWebKey;
-import org.apache.cxf.rs.security.jose.jwk.JsonWebKeys;
-import org.apache.cxf.rs.security.jose.jwk.JwkUtils;
-import org.apache.cxf.rs.security.jose.jws.JwsCompactProducer;
-import org.apache.cxf.rs.security.jose.jws.JwsHeaders;
-import org.apache.cxf.rs.security.jose.jws.JwsSignatureProvider;
-import org.apache.cxf.rs.security.jose.jws.PrivateKeyJwsSignatureProvider;
-import org.apache.cxf.rs.security.jose.jws.JwsUtils;
-
 
 /**
  * Created by epuzanov on 8/30/21.
@@ -61,11 +36,6 @@ public class JsonPortableExporter implements Exporter<Substance> {
     private static final String LEADING_HEADER= "\t\t";
     private final ObjectWriter writer =  EntityFactory.EntityMapper.FULL_ENTITY_MAPPER().writer();
     private static final List<String> fieldsToRemove = Arrays.asList("_name","_nameHTML","_formulaHTML","_approvalIDDisplay","_isClassification","_self","self","approvalID","approved","approvedBy","changeReason","created","createdBy","lastEdited","lastEditedBy","deprecated","uuid","refuuid","originatorUuid","linkingID","id","documentDate","status","version");
-    private static final JsonWebKeys jwks = loadJwks("export.jwks");
-    private static final String privateKeyId = findPrivateKeyId();
-    private static final ContentAlgorithm enc = ContentAlgorithm.A256GCM;
-    private static final KeyAlgorithm alg = KeyAlgorithm.RSA_OAEP;
-    private static final SignatureAlgorithm sig = SignatureAlgorithm.RS256;
     private static final String gsrsVersion = "3.0.2";
     private static final boolean sign = false;
 
@@ -88,89 +58,19 @@ public class JsonPortableExporter implements Exporter<Substance> {
     }
 
     private static String makePortable(JsonNode tree) {
-        String privKeyId = findPrivateKeyId();
-        ObjectMapper mapper = new ObjectMapper();
-        JwsHeaders protectedHeaders = new JwsHeaders();
-        protectedHeaders.setKeyId(privateKeyId);
-        protectedHeaders.setContentType("application/json");
-        JsonNode ori = tree.get("_self");
-        if (ori == null) {
-            ori = new TextNode("Unknown");
-        }
-        protectedHeaders.setHeader("ori", ori.asText());
-        protectedHeaders.setHeader("ver", gsrsVersion);
-        protectedHeaders.setHeader("dat", ZonedDateTime.now(ZoneOffset.UTC).format(DateTimeFormatter.ISO_INSTANT));
         deleteValidationNotes((ObjectNode) tree);
         uuidToIndex(tree);
         scrub(tree);
         checkAccess(tree);
         String out = tree.toString();
         if (sign) {
-            JwsCompactProducer jwsProducer = new JwsCompactProducer(protectedHeaders, out);
-            JwsSignatureProvider jwsp = JwsUtils.getSignatureProvider(jwks.getKey(privateKeyId), sig);
-            out = jwsProducer.signWith(jwsp);
+            Map<String, Object> headers = new HashMap<String, Object>();
+            headers.put("ori", tree.hasNonNull("_self") ? tree.get("_self").asText() : "Unknown");
+            headers.put("ver", gsrsVersion);
+            headers.put("dat", ZonedDateTime.now(ZoneOffset.UTC).format(DateTimeFormatter.ISO_INSTANT));
+            out = JoseUtil.getInstance().sign(out, headers);
         }
         return out;
-    }
-
-    private static void encrypt(JsonNode node) {
-        JsonWebKey key;
-        KeyEncryptionProvider keyEncryption;
-        ObjectMapper mapper = new ObjectMapper();
-        JsonNode result = mapper.createObjectNode();
-        JweHeaders protectedHeaders = new JweHeaders(enc);
-        protectedHeaders.setType(JoseType.JOSE_JSON);
-        JweHeaders sharedUnprotectedHeaders = new JweHeaders();
-        sharedUnprotectedHeaders.setKeyEncryptionAlgorithm(alg);
-        ContentEncryptionProvider contentEncryption = JweUtils.getContentEncryptionProvider(enc, true);
-        List<JweEncryptionProvider> jweProviders = new LinkedList<JweEncryptionProvider>();
-        List<JweHeaders> perRecipientHeades = new LinkedList<JweHeaders>();
-        if (privateKeyId != null && node.get("access").findValue(privateKeyId) == null) {
-            keyEncryption = JweUtils.getKeyEncryptionProvider(jwks.getKey(privateKeyId), alg);
-            jweProviders.add(new JweEncryption(keyEncryption, contentEncryption));
-            perRecipientHeades.add(new JweHeaders(privateKeyId));
-        }
-        Iterator<JsonNode> it = node.get("access").elements();
-        while (it.hasNext()) {
-            key = jwks.getKey(it.next().asText());
-            if (key != null) {
-                keyEncryption = JweUtils.getKeyEncryptionProvider(key, alg);
-                jweProviders.add(new JweEncryption(keyEncryption, contentEncryption));
-                perRecipientHeades.add(new JweHeaders(key.getKeyId()));
-            }
-        }
-        if (!jweProviders.isEmpty()) {
-            JweJsonProducer p = new JweJsonProducer(protectedHeaders,
-                                        sharedUnprotectedHeaders,
-                                        StringUtils.toBytesUTF8(node.toString()));
-            try {
-                result = mapper.readTree(p.encryptWith(jweProviders, perRecipientHeades));
-            } catch (Exception e) {
-            }
-        }
-        ((ObjectNode) node).removeAll();
-        Iterator<Map.Entry<String, JsonNode>> fields = result.fields();
-        while (fields.hasNext()) {
-            Map.Entry<String, JsonNode> field = fields.next();
-            ((ObjectNode) node).set(field.getKey(), field.getValue());
-        }
-    }
-
-    private static JsonWebKeys loadJwks(String fileName) {
-        try {
-            return JwkUtils.readJwkSet(new String(Files.readAllBytes(Paths.get(fileName))));
-        } catch (Exception e) {
-            return new JsonWebKeys();
-        }
-    }
-
-    private static String findPrivateKeyId () {
-        return  jwks.getKeys()
-                    .stream()
-                    .filter(k->k.getKeyProperty(JsonWebKey.RSA_PRIVATE_EXP) != null)
-                    .map(k->k.getKeyId())
-                    .findFirst()
-                    .orElse(null);
     }
 
     private static void uuidToIndex (JsonNode node) {
@@ -228,7 +128,7 @@ public class JsonPortableExporter implements Exporter<Substance> {
                 checkAccess(node.get(key));
             }
             if (node.has("access") && !node.get("access").isEmpty()) {
-                encrypt(node);
+                JoseUtil.getInstance().encrypt((ObjectNode)node);
             }
         } else if (node.isArray()) {
             Iterator<JsonNode> it = node.elements();
@@ -243,5 +143,4 @@ public class JsonPortableExporter implements Exporter<Substance> {
             ((ObjectNode) n).remove(fieldsToRemove);
         }
     }
-
 }
