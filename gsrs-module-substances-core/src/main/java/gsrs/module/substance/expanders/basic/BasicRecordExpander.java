f45a9775dc6e2096d44b822954b05a377fc4967f
Mitch Miller <mitch.miller@thinkscience.us>
2022-10-06 14:24:08 -0400

    WIP:Expander unit tests and data file

--- gsrs-module-substances-core/src/main/java/gsrs/module/substance/expanders/basic/BasicRecordExpander.java
+++ gsrs-module-substances-core/src/main/java/gsrs/module/substance/expanders/basic/BasicRecordExpander.java
@@ -12,6 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
 import java.util.function.Consumer;
@@ -71,7 +72,6 @@ public class BasicRecordExpander implements RecordExpander<ix.ginas.models.v1.Su
 
     private void appendDefinitionalSubstances(Substance substance, Consumer<Substance> consumer, int generation) {
         log.trace("starting in appendDefinitionalSubstances, generation: {}", generation);
-        generation++;
         for (SubstanceReference ref : substance.getDependsOnSubstanceReferences()) {
             Substance referred = substanceRepository.findBySubstanceReference(ref);
 
@@ -80,11 +80,16 @@ public class BasicRecordExpander implements RecordExpander<ix.ginas.models.v1.Su
                         ref.refuuid);
             } else {
                 //may be necessary to retrieve entire substance?
-                referred =substanceEntityService.get(referred.uuid).get();
+                Optional<Substance> optionalSubstance = substanceEntityService.get(referred.uuid);
+                if( !optionalSubstance.isPresent()){
+                    log.error("Error retrieving complete substance!");
+                    continue;
+                }
+                referred =optionalSubstance.get();
                 //log.trace("accepting substance {}", referred.uuid.toString());
                 consumer.accept(referred);
-                if (generation <= generationsToExpand) {
-                    appendDefinitionalSubstances(referred, consumer, generation);
+                if (generation < generationsToExpand) {
+                    appendDefinitionalSubstances(referred, consumer, generation+1);
                 }
             }
         }
@@ -92,16 +97,22 @@ public class BasicRecordExpander implements RecordExpander<ix.ginas.models.v1.Su
 
     private void appendRelatedSubstances(Substance substance, Consumer<Substance> consumer, int generation) {
         log.trace("starting in appendRelatedSubstances, generation: {}", generation);
-        generation++;
         for (Relationship rel : substance.relationships) {
 
             if (rel.relatedSubstance != null && rel.relatedSubstance.refuuid != null) {
                 log.trace("looking for substance with UUID {}", rel.relatedSubstance.refuuid);
                 Substance relatedSubstance = substanceRepository.findBySubstanceReference(rel.relatedSubstance);
                 if(relatedSubstance!=null) {
+                    //may be necessary to retrieve entire substance?
+                    Optional<Substance> optionalSubstance=substanceEntityService.get(relatedSubstance.uuid);
+                    if(!optionalSubstance.isPresent()){
+                        log.error("Error retrieving substance!");
+                        continue;
+                    }
+                    relatedSubstance =optionalSubstance.get();
                     consumer.accept(relatedSubstance);
-                    if (generation <= generationsToExpand) {
-                        appendRelatedSubstances(relatedSubstance, consumer, generation);
+                    if (generation < generationsToExpand) {
+                        appendRelatedSubstances(relatedSubstance, consumer, generation+1);
                     }
                 }
                 else {
@@ -111,9 +122,16 @@ public class BasicRecordExpander implements RecordExpander<ix.ginas.models.v1.Su
             if( rel.mediatorSubstance!= null ){
                 Substance mediatorSubstance = substanceRepository.findBySubstanceReference (rel.mediatorSubstance);
                 if(mediatorSubstance!=null) {
+                    log.trace("We have a mediator sub: {}", mediatorSubstance.uuid.toString());
+                    Optional<Substance> optionalSubstance=substanceEntityService.get(mediatorSubstance.uuid);
+                    if(!optionalSubstance.isPresent()){
+                        log.error("Error retrieving substance!");
+                        continue;
+                    }
+                    mediatorSubstance = optionalSubstance.get();
                     consumer.accept(mediatorSubstance);
-                    if (generation <= generationsToExpand) {
-                        appendRelatedSubstances(mediatorSubstance, consumer, generation);
+                    if (generation < generationsToExpand) {
+                        appendRelatedSubstances(mediatorSubstance, consumer, generation+1);
                     }
                 } else {
                     log.warn("Error retrieving related substance with UUID {}", rel.mediatorSubstance.refuuid);
