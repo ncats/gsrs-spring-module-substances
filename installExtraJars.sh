9c7f57930ee8f381571cb5828c53172be9e017a0
Hu <hul5@nih.gov>
2022-12-07 12:27:13 -0500

    test travis

--- installExtraJars.sh
+++ installExtraJars.sh
@@ -1,3 +1,4 @@
+# this is a test
 ./mvnw install:install-file -Dfile=extraJars/applications-api-3.0.3-SNAPSHOT.jar
 ./mvnw install:install-file -Dfile=extraJars/products-api-3.0.3-SNAPSHOT.jar
 ./mvnw install:install-file -Dfile=extraJars/clinical-trials-api-3.0.3-SNAPSHOT.jar
