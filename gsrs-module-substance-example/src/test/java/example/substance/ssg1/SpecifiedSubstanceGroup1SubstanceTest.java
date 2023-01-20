0aa0e01cd0b5ecfa71e78006b4a47cf02bdea2a7
Mitch Miller <mitch.miller@thinkscience.us>
2023-01-20 16:13:17 -0500

    Additional unit test

--- gsrs-module-substance-example/src/test/java/example/substance/ssg1/SpecifiedSubstanceGroup1SubstanceTest.java
+++ gsrs-module-substance-example/src/test/java/example/substance/ssg1/SpecifiedSubstanceGroup1SubstanceTest.java
@@ -26,4 +26,17 @@ public class SpecifiedSubstanceGroup1SubstanceTest {
         Assertions.assertNotNull(defs);//just getting here with no NPE proves the 20-January-2023 fix works
 
     }
+
+    @Test
+    public void SSG1getDependsOnSubstanceReferencesTest() {
+        SubstanceBuilder basicSubstancBuilder = new SubstanceBuilder();
+        basicSubstancBuilder.addName("SSG1 Basis");
+        Substance basicSubstance = basicSubstancBuilder.build();
+        SpecifiedSubstanceGroup1SubstanceBuilder builder = new SpecifiedSubstanceGroup1SubstanceBuilder(basicSubstance);
+        //intentionally laving definition blank
+        SpecifiedSubstanceGroup1Substance  substance = builder.build();
+        List<SubstanceReference> refs= substance.getDependsOnSubstanceReferences();
+        Assertions.assertNotNull(refs);//just getting here with no NPE proves the 20-January-2023 fix works
+
+    }
 }
