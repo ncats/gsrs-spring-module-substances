8076c65fb35e6f686bc87ebbae68ea5d3684b1cc
Hu <hul5@nih.gov>
2023-02-17 13:22:51 -0500

    testing

--- gsrs-module-substances-core/src/main/java/gsrs/module/substance/indexers/BulkSearchResultKeyListIndexValueMaker.java
+++ gsrs-module-substances-core/src/main/java/gsrs/module/substance/indexers/BulkSearchResultKeyListIndexValueMaker.java
@@ -2,7 +2,9 @@ package gsrs.module.substance.indexers;
 
 import java.util.List;
 import java.util.function.Consumer;
+
 import org.springframework.beans.factory.annotation.Autowired;
+
 import gsrs.repository.BulkSearchResultKeyRepository;
 import gsrs.repository.PrincipalRepository;
 import gsrs.security.GsrsSecurityUtils;
@@ -12,7 +14,9 @@ import ix.core.search.text.IndexValueMaker;
 import ix.core.search.text.IndexableValue;
 import ix.core.util.EntityUtils.EntityWrapper;
 import ix.ginas.models.v1.Substance;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class BulkSearchResultKeyListIndexValueMaker implements IndexValueMaker<Substance> {
 	
 	@Autowired
@@ -27,20 +31,26 @@ public class BulkSearchResultKeyListIndexValueMaker implements IndexValueMaker<S
         return Substance.class;
     }
 	
+	
 	@Override
     public void createIndexableValues(Substance substance, Consumer<IndexableValue> consumer) {
-			
+		
+				
     	if(!GsrsSecurityUtils.getCurrentUsername().isPresent()) return;  	    	
     	String userName = GsrsSecurityUtils.getCurrentUsername().get();
+    	log.warn("BulkSearchResultKeyListIndexValueMaker " + " username: " + userName);
     	Principal user = principalRepository.findDistinctByUsernameIgnoreCase(userName);
 		if(user == null)
 			return;
-		String key = EntityWrapper.of(substance).getKey().toRootKey().toString();
-		
+		String key = EntityWrapper.of(substance).getKey().toRootKey().getIdString();
+	
 		List<String> list = bulkSearchResultKeyRepository.getAllListNamesFromKey(key, user.id);
+		list.forEach(e->log.warn("list name " + e));
 		
 		list.forEach(listName -> {			
-			consumer.accept(IndexableValue.simpleFacetStringValue("User List",BulkSearchResultService.getIndexedValue(userName, listName) ));});
+			String value = BulkSearchResultService.getIndexedValue(userName, listName);
+			log.warn("value: " + value);
+			consumer.accept(IndexableValue.simpleFacetStringValue("User List",value));});
 	
 	}
 }
