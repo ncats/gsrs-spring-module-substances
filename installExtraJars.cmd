07e7362e838cdc63596d5802e5ef79c49b4a2fe1
A. Welsch <alexander.welsch@fda.hhs.gov>
2023-03-21 18:12:06 -0400

    change fda extension version products api number

--- installExtraJars.cmd
+++ installExtraJars.cmd
@@ -1,4 +1,5 @@
 mvnw.cmd install:install-file -Dfile=extraJars/applications-api-3.0.3-SNAPSHOT.jar
-mvnw.cmd install:install-file -Dfile=extraJars/products-api-3.0.3-SNAPSHOT.jar
+mvnw.cmd install:install-file -Dfile=extraJars/products-api-3.1-SNAPSHOT.jar
 mvnw.cmd install:install-file -Dfile=extraJars/clinical-trials-api-3.1-SNAPSHOT.jar
 mvnw.cmd install:install-file -Dfile=extraJars/molwitch-renderer-1.0.12-SNAPSHOT.jar
+
--- installExtraJars.sh
+++ installExtraJars.sh
@@ -1,4 +1,4 @@
 ./mvnw install:install-file -Dfile=extraJars/applications-api-3.0.3-SNAPSHOT.jar
-./mvnw install:install-file -Dfile=extraJars/products-api-3.0.3-SNAPSHOT.jar
+./mvnw install:install-file -Dfile=extraJars/products-api-3.1-SNAPSHOT.jar
 ./mvnw install:install-file -Dfile=extraJars/clinical-trials-api-3.1-SNAPSHOT.jar
 ./mvnw install:install-file -Dfile=extraJars/molwitch-renderer-1.0.12-SNAPSHOT.jar
