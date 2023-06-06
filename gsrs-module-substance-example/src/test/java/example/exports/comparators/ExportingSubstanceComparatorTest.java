0d8efc67e1e1de84c80e99faab00582502444028
Mitch Miller <mitch.miller@thinkscience.us>
2023-06-06 13:01:02 -0400

    additional unit test

--- gsrs-module-substance-example/src/test/java/example/exports/comparators/ExportingSubstanceComparatorTest.java
+++ gsrs-module-substance-example/src/test/java/example/exports/comparators/ExportingSubstanceComparatorTest.java
@@ -1,19 +1,24 @@
 package example.exports.comparators;
 
 import gsrs.module.substance.comparators.ExportingSubstanceComparator;
+import ix.core.models.Keyword;
 import ix.ginas.modelBuilders.ChemicalSubstanceBuilder;
 import ix.ginas.modelBuilders.PolymerSubstanceBuilder;
+import ix.ginas.modelBuilders.StructurallyDiverseSubstanceBuilder;
+import ix.ginas.models.EmbeddedKeywordList;
 import ix.ginas.models.v1.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 public class ExportingSubstanceComparatorTest {
 
     //case 1: no dependencies - expect result 0
     @Test
-    public void testSameOrder(){
+    public void testSameOrder() {
         ChemicalSubstanceBuilder chemicalSubstanceBuilder1 = new ChemicalSubstanceBuilder();
         chemicalSubstanceBuilder1.setStructureWithDefaultReference("c1ccccc1C");
         chemicalSubstanceBuilder1.setDisplayName("Toluene");
@@ -31,7 +36,7 @@ public class ExportingSubstanceComparatorTest {
 
     //case 1: no dependencies - expect result 0 (Assign IDs)
     @Test
-    public void testSameOrderWithIds(){
+    public void testSameOrderWithIds() {
         ChemicalSubstanceBuilder chemicalSubstanceBuilder1 = new ChemicalSubstanceBuilder();
         chemicalSubstanceBuilder1.setStructureWithDefaultReference("c1ccccc1C");
         chemicalSubstanceBuilder1.setDisplayName("Toluene");
@@ -50,7 +55,7 @@ public class ExportingSubstanceComparatorTest {
     }
 
     @Test
-    public void testDep1(){
+    public void testDep1() {
         ChemicalSubstanceBuilder chemicalSubstanceBuilder2 = new ChemicalSubstanceBuilder();
         chemicalSubstanceBuilder2.setStructureWithDefaultReference("c1ccccc1C=C");
         chemicalSubstanceBuilder2.setDisplayName("Styrene");
@@ -63,13 +68,13 @@ public class ExportingSubstanceComparatorTest {
 
         Polymer innerPolymer = new Polymer();
         Material monomer = new Material();
-        monomer.monomerSubstance= new SubstanceReference();
+        monomer.monomerSubstance = new SubstanceReference();
         monomer.monomerSubstance.wrappedSubstance = monomerSubstance;
-        monomer.monomerSubstance.refuuid=monomerSubstance.getUuid().toString();
-        monomer.defining=true;
+        monomer.monomerSubstance.refuuid = monomerSubstance.getUuid().toString();
+        monomer.defining = true;
         innerPolymer.monomers.add(monomer);
         innerPolymer.displayStructure = new GinasChemicalStructure();
-        innerPolymer.displayStructure.molfile="\n" +
+        innerPolymer.displayStructure.molfile = "\n" +
                 "  ACCLDraw06052322162D\n" +
                 "\n" +
                 " 10 10  0  0  0  0  0  0  0  0999 V2000\n" +
@@ -104,7 +109,7 @@ public class ExportingSubstanceComparatorTest {
                 "M  END\n";
         polymerBuilder.setPolymer(innerPolymer);
 
-        PolymerSubstance polymerSubstance=polymerBuilder.build();
+        PolymerSubstance polymerSubstance = polymerBuilder.build();
 
         ExportingSubstanceComparator comparator = new ExportingSubstanceComparator();
         int result = comparator.compare(polymerSubstance, monomerSubstance);
@@ -112,7 +117,7 @@ public class ExportingSubstanceComparatorTest {
     }
 
     @Test
-    public void testDepRev(){
+    public void testDepRev() {
         ChemicalSubstanceBuilder chemicalSubstanceBuilder2 = new ChemicalSubstanceBuilder();
         chemicalSubstanceBuilder2.setStructureWithDefaultReference("c1ccccc1C=C");
         chemicalSubstanceBuilder2.setDisplayName("Styrene");
@@ -125,13 +130,13 @@ public class ExportingSubstanceComparatorTest {
 
         Polymer innerPolymer = new Polymer();
         Material monomer = new Material();
-        monomer.monomerSubstance= new SubstanceReference();
+        monomer.monomerSubstance = new SubstanceReference();
         monomer.monomerSubstance.wrappedSubstance = monomerSubstance;
-        monomer.monomerSubstance.refuuid=monomerSubstance.getUuid().toString();
-        monomer.defining=true;
+        monomer.monomerSubstance.refuuid = monomerSubstance.getUuid().toString();
+        monomer.defining = true;
         innerPolymer.monomers.add(monomer);
         innerPolymer.displayStructure = new GinasChemicalStructure();
-        innerPolymer.displayStructure.molfile="\n" +
+        innerPolymer.displayStructure.molfile = "\n" +
                 "  ACCLDraw06052322162D\n" +
                 "\n" +
                 " 10 10  0  0  0  0  0  0  0  0999 V2000\n" +
@@ -166,11 +171,52 @@ public class ExportingSubstanceComparatorTest {
                 "M  END\n";
         polymerBuilder.setPolymer(innerPolymer);
 
-        PolymerSubstance polymerSubstance=polymerBuilder.build();
+        PolymerSubstance polymerSubstance = polymerBuilder.build();
 
         ExportingSubstanceComparator comparator = new ExportingSubstanceComparator();
-        int result = comparator.compare( monomerSubstance, polymerSubstance);
+        int result = comparator.compare(monomerSubstance, polymerSubstance);
         Assertions.assertEquals(-1, result);
     }
 
+
+    @Test
+    public void testStrDivDep() {
+        StructurallyDiverseSubstanceBuilder wholePlantBuilder = new StructurallyDiverseSubstanceBuilder();
+        wholePlantBuilder.addName("Himalayan maple");
+        StructurallyDiverse inner1 = new StructurallyDiverse();
+        inner1.organismFamily = "Sapindaceae";
+        inner1.organismGenus = "Acer";
+        inner1.organismSpecies = "caesium";
+        inner1.sourceMaterialClass = "ORGANISM";
+        inner1.sourceMaterialType = "PLANT";
+        inner1.part = new EmbeddedKeywordList();
+        inner1.part.add(new Keyword("WHOLE"));
+        wholePlantBuilder.setStructurallyDiverse(inner1);
+        wholePlantBuilder.setUUID(UUID.randomUUID());
+        StructurallyDiverseSubstance wholePlant = wholePlantBuilder.build();
+
+        StructurallyDiverseSubstanceBuilder leafBuilder = new StructurallyDiverseSubstanceBuilder();
+        leafBuilder.addName("Maple leaf");
+        StructurallyDiverse inner2 = new StructurallyDiverse();
+        inner2.part = new EmbeddedKeywordList();
+        inner2.part.add(new Keyword("Leaf"));
+        inner1.organismFamily = "Sapindaceae";
+        inner2.organismGenus = "Acer";
+        inner2.organismSpecies = "caesium";
+        inner2.sourceMaterialClass = "ORGANISM";
+        inner2.sourceMaterialType = "PLANT";
+        inner2.parentSubstance = new SubstanceReference();
+        inner2.parentSubstance.wrappedSubstance = wholePlant;
+        inner2.parentSubstance.refuuid = wholePlant.getUuid().toString();
+        leafBuilder.setStructurallyDiverse(inner2);
+        leafBuilder.setUUID(UUID.randomUUID());
+        StructurallyDiverseSubstance leaf = leafBuilder.build();
+
+        List<Substance> substances = new ArrayList<>();
+        substances.add(leaf);
+        substances.add(wholePlant);
+        ExportingSubstanceComparator comparator = new ExportingSubstanceComparator();
+        substances.sort(comparator);
+        Assertions.assertEquals(wholePlant, substances.get(0));
+    }
 }
