630beb648c8c10550d2ead7bd021529632a4377f
Magnus Blomberg <magnus.blomberg@who-umc.org>
2023-06-22 15:23:38 +0200

    PostConstruct method for init conf and HttpClient

--- gsrs-module-substances-core/src/main/java/ix/ginas/utils/ExternalIDGenerator.java
+++ gsrs-module-substances-core/src/main/java/ix/ginas/utils/ExternalIDGenerator.java
@@ -3,11 +3,14 @@ package ix.ginas.utils;
 import gsrs.module.substance.repository.SubstanceRepository;
 import ix.ginas.models.v1.Substance;
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.*;
 import java.util.Map;
+
+import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -16,6 +19,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ConfigurationProperties("ix.ginas.approval-id-generator")
 @Data
+@Slf4j
 public class ExternalIDGenerator implements SubstanceApprovalIdGenerator{
     public static final String PARAMETER_ERROR_MESSAGE = 
         "ExternalIDGenerator requires configuration properties in ix.ginas.approvalIdGenerator.externalIDGenerator";
@@ -26,23 +30,33 @@ public class ExternalIDGenerator implements SubstanceApprovalIdGenerator{
     public static final String VALIDATE_ERROR_MESSAGE = 
         "There was a problem validating approvalID from remote service. Status code was: %s";
 
-
     private Map<String, Object> externalIDGenerator;
 
-	@Autowired
+    private String generateUrl;
+    private String validateUrl;
+    private String accesstoken;
+
+    private HttpClient httpClient;
+
+    @Autowired
     private SubstanceRepository substanceRepository;
-               
+
+    @PostConstruct
+    private void initialize() {
+        if(externalIDGenerator == null){
+            throw new RuntimeException(PARAMETER_ERROR_MESSAGE);
+        }
+        generateUrl = externalIDGenerator.get("generateUrl").toString();
+        validateUrl = externalIDGenerator.get("validateUrl").toString();
+        accesstoken = externalIDGenerator.get("accesstoken").toString();
+        httpClient = HttpClient.newHttpClient();
+    }
+
 	public String getName(){
 		return this.getClass().getSimpleName();
 	}
 
 	public String generateId(Substance obj) {      
-        if(externalIDGenerator == null){
-            throw new RuntimeException(PARAMETER_ERROR_MESSAGE);
-        }
-
-        HttpClient httpClient = HttpClient.newHttpClient();
-
         String id = "";
         try {
             HttpResponse<String> response = httpClient.send(CreateGenerateHttpRequest(), HttpResponse.BodyHandlers.ofString());
@@ -55,20 +69,13 @@ public class ExternalIDGenerator implements SubstanceApprovalIdGenerator{
 			}
         } 
         catch (IOException | InterruptedException e) {
-            e.printStackTrace();
+            log.error(e.getMessage(), e);
             throw new RuntimeException(e.getMessage());
         }
-
         return id;
 	}
                
 	public boolean isValidId(String id){
-        if(externalIDGenerator == null){
-            throw new RuntimeException(PARAMETER_ERROR_MESSAGE);
-        }
-
-        HttpClient httpClient = HttpClient.newHttpClient();
-
         boolean isValid = false;
         try {
             HttpResponse<String> response = httpClient.send(CreateValidateHttpRequest(id), HttpResponse.BodyHandlers.ofString());
@@ -81,18 +88,15 @@ public class ExternalIDGenerator implements SubstanceApprovalIdGenerator{
 			}
         } 
         catch (IOException | InterruptedException e) {
-            e.printStackTrace();
+            log.error(e.getMessage(), e);
             throw new RuntimeException(e.getMessage());
         }
         return isValid && !substanceRepository.existsByApprovalID(id);
 	}
 
     private HttpRequest CreateGenerateHttpRequest(){
-		String url = externalIDGenerator.get("generateUrl").toString();
-        String accesstoken = externalIDGenerator.get("accesstoken").toString();
-
         HttpRequest.Builder requestBuilder = HttpRequest.newBuilder()
-            .uri(URI.create(url))
+            .uri(URI.create(generateUrl))
             .header("Content-Type", "application/json")
             .POST(HttpRequest.BodyPublishers.noBody());
         
@@ -104,12 +108,10 @@ public class ExternalIDGenerator implements SubstanceApprovalIdGenerator{
 	}
 
     private HttpRequest CreateValidateHttpRequest(String id){
-		String url = externalIDGenerator.get("validateUrl").toString();
-        String accesstoken = externalIDGenerator.get("accesstoken").toString();
         String requestBody = String.format("{\"ApprovalId\": \"%s\"}", id);
 
         HttpRequest.Builder requestBuilder = HttpRequest.newBuilder()
-            .uri(URI.create(url))
+            .uri(URI.create(validateUrl))
             .header("Content-Type", "application/json")
             .POST(HttpRequest.BodyPublishers.ofString(requestBody));
         
