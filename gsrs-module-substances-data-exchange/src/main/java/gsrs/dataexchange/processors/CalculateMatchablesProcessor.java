eab8bbfce01aa38825241cdb7f1dd9e77b938f4d
Mitch Miller <mitch.miller@thinkscience.us>
2023-07-07 17:25:26 -0400

    use a transaction in preremove

--- gsrs-module-substances-data-exchange/src/main/java/gsrs/dataexchange/processors/CalculateMatchablesProcessor.java
+++ gsrs-module-substances-data-exchange/src/main/java/gsrs/dataexchange/processors/CalculateMatchablesProcessor.java
@@ -58,7 +58,10 @@ public class CalculateMatchablesProcessor implements EntityProcessor<Substance> 
     @Override
     public void preRemove(Substance obj) {
         log.trace("preRemove");
-        keyValueMappingRepository.deleteByRecordId(obj.uuid);
+        TransactionTemplate tx = new TransactionTemplate(platformTransactionManager);
+        log.trace("got tx " + tx);
+        tx.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRES_NEW);
+        tx.executeWithoutResult(x->keyValueMappingRepository.deleteByRecordId(obj.uuid));
     }
 
     @Override
@@ -123,9 +126,9 @@ public class CalculateMatchablesProcessor implements EntityProcessor<Substance> 
         TransactionTemplate tx = new TransactionTemplate(platformTransactionManager);
         log.trace("got tx " + tx);
         tx.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRES_NEW);
-//        tx.executeWithoutResult(
-//                a -> keyValueMappingRepository.deleteByRecordId(substance.uuid)
-//        );
+        tx.executeWithoutResult(
+                a -> keyValueMappingRepository.deleteByRecordId(substance.uuid)
+        );
         log.trace("just completed call to deleteByRecordId");
 
         Set<UUID> importMetadataRecordsToReindex = new HashSet<>();
