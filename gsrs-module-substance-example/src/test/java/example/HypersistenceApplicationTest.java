60da7e8edd78421280469ae5e9c50e232d45e3bf
epuzanov <epuzanov@users.noreply.github.com>
2023-09-22 19:49:24 +0200

    hypersistence support

---
+++ gsrs-module-substance-example/src/test/java/example/HypersistenceApplicationTest.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2002-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package example;
+
+import io.hypersistence.optimizer.HypersistenceOptimizer;
+import io.hypersistence.optimizer.core.event.Event;
+import io.hypersistence.optimizer.hibernate.event.configuration.connection.Connection9071557016Event;
+import io.hypersistence.optimizer.hibernate.event.configuration.query.Query7786258879Event;
+import io.hypersistence.optimizer.hibernate.event.configuration.query.Query2468924130Event;
+import io.hypersistence.optimizer.hibernate.event.configuration.schema.SchemaGenerationEvent;
+import io.hypersistence.optimizer.hibernate.event.mapping.association.Association3890524098Event;
+import io.hypersistence.optimizer.hibernate.event.mapping.association.Association934543432Event;
+import io.hypersistence.optimizer.hibernate.event.mapping.association.OneToOneWithoutMapsIdEvent;
+import io.hypersistence.optimizer.hibernate.event.mapping.association.fetching.EagerFetchingEvent;
+import io.hypersistence.optimizer.hibernate.event.query.QueryEvent;
+import io.hypersistence.optimizer.hibernate.event.session.SessionEvent;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.TransactionException;
+import org.springframework.transaction.support.TransactionCallback;
+import org.springframework.transaction.support.TransactionTemplate;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.junit.Assert.*;
+
+@SpringBootTest
+public class ApplicationTest {
+
+    protected final Logger LOGGER = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    private HypersistenceOptimizer hypersistenceOptimizer;
+
+    @Autowired
+    private TransactionTemplate transactionTemplate;
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Test
+    public void test() throws ExecutionException, InterruptedException {
+        assertEventTriggered(2, EagerFetchingEvent.class);
+        assertEventTriggered(1, Association3890524098Event.class);
+        assertEventTriggered(1, Association934543432Event.class);
+        assertEventTriggered(1, OneToOneWithoutMapsIdEvent.class);
+        assertEventTriggered(1, Connection9071557016Event.class);
+        assertEventTriggered(1, SchemaGenerationEvent.class);
+        assertEventTriggered(1, Query2468924130Event.class);
+        assertEventTriggered(1, Query7786258879Event.class);
+        assertEventTriggered(1, SessionEvent.class);
+    }
+
+    protected void assertEventTriggered(int expectedCount, Class<? extends Event> eventClass) {
+        int count = 0;
+
+        for (Event event : hypersistenceOptimizer.getEvents()) {
+            if (event.getClass().equals(eventClass)) {
+                count++;
+            }
+        }
+
+        assertSame(expectedCount, count);
+    }
+}
