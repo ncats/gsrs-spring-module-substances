c141e977968004aa54e80a5eb4b8aac50765bb66
alx652 <welscha2@nih.gov>
2023-10-06 13:22:37 -0400

    add config options to task initializer

--- gsrs-module-substances-core/src/main/java/gsrs/module/substance/tasks/NameStandardizerTaskInitializer.java
+++ gsrs-module-substances-core/src/main/java/gsrs/module/substance/tasks/NameStandardizerTaskInitializer.java
@@ -8,20 +8,20 @@ import java.io.PrintStream;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
+import gov.nih.ncats.common.util.TimeUtil;
+import gsrs.EntityPersistAdapter;
+import gsrs.GsrsEntityProcessorListener;
+import ix.core.EntityFetcher;
+import ix.core.util.EntityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.TransactionDefinition;
 import org.springframework.transaction.support.TransactionTemplate;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
-
-import gov.nih.ncats.common.executors.BlockingSubmitExecutor;
 import gsrs.config.FilePathParserUtils;
 import gsrs.module.substance.repository.NameRepository;
 import gsrs.module.substance.repository.SubstanceRepository;
@@ -35,23 +35,27 @@ import ix.ginas.models.v1.Name;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
+import javax.persistence.EntityManager;
+
 @Slf4j
 @Data
 public class NameStandardizerTaskInitializer extends ScheduledTaskInitializer {
 
     private String regenerateNameValue = "";
     private Boolean forceRecalculationOfAll =false;
-    
+
     @JsonIgnore
     private DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_DATE;
     @JsonIgnore
     private DateTimeFormatter formatterTime = DateTimeFormatter.ofPattern("HH-mm-ss");
-    
-    
+
+
     private String outputPath;
     private String name = "nameStandardizationReport";
     private String STANDARD_FILE_ENCODING ="UTF-8";
     private String description;
+    private boolean disabledHistory = false;
+    private boolean disabledHooks = false;
 
     @Autowired
     private SubstanceRepository substanceRepository;
@@ -66,9 +70,9 @@ public class NameStandardizerTaskInitializer extends ScheduledTaskInitializer {
     private PlatformTransactionManager platformTransactionManager;
 
 
-	@Autowired
-	private NameStandardizerConfiguration nameStdConfig;
-	
+    @Autowired
+    private NameStandardizerConfiguration nameStdConfig;
+
     private NameStandardizer stdNameStandardizer;
 
     @JsonProperty("formatter")
@@ -77,30 +81,30 @@ public class NameStandardizerTaskInitializer extends ScheduledTaskInitializer {
             formatter = DateTimeFormatter.ofPattern(format);
         }
     }
-    
 
     @JsonProperty("formatterTime")
     public void setFormatTime(String format) {
         if(format !=null){
-        	formatterTime = DateTimeFormatter.ofPattern(format);
+            formatterTime = DateTimeFormatter.ofPattern(format);
         }
     }
-    
+
     private void initIfNeeded() {
-    	try {
-	    	if(stdNameStandardizer==null) {
-	    		if(nameStdConfig!=null) {
-	    			stdNameStandardizer=nameStdConfig.stdNameStandardizer();
-	    		}
-	    	}
-    	}catch(Exception e) {
-    		log.warn("trouble instantiating name standardizer", e);
-    	}
+        try {
+            if(stdNameStandardizer==null) {
+                if(nameStdConfig!=null) {
+                    stdNameStandardizer=nameStdConfig.stdNameStandardizer();
+                }
+            }
+        }catch(Exception e) {
+            log.warn("trouble instantiating name standardizer", e);
+        }
     }
-    
+
     @Override
     public void run(SchedulerPlugin.JobStats stats, SchedulerPlugin.TaskListener l) {
-    	initIfNeeded();
+        long start = TimeUtil.getCurrentTimeMillis();
+        initIfNeeded();
         File writeFile = getOutputFile();
         File abfile = writeFile.getAbsoluteFile();
         File pfile = abfile.getParentFile();
@@ -113,7 +117,6 @@ public class NameStandardizerTaskInitializer extends ScheduledTaskInitializer {
 
         l.message("Initializing name standardization: acquiring list");
 
-        ExecutorService executor = BlockingSubmitExecutor.newFixedThreadPool(5, 10);
         l.message("Initializing name standardization: acquiring user account");
         Authentication adminAuth = adminService.getAnyAdmin();
         l.message("Initializing name standardization: starting process");
@@ -122,9 +125,6 @@ public class NameStandardizerTaskInitializer extends ScheduledTaskInitializer {
         try (PrintStream out = makePrintStream(writeFile)){
             out.print("Existing standardized name\tNew standardized name\tMessage\n");
             adminService.runAs(adminAuth, (Runnable) () -> {
-                TransactionTemplate tx = new TransactionTemplate(platformTransactionManager);
-                log.trace("got outer tx " + tx);
-                tx.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRES_NEW);
                 try {
                     processNames(l, out);
                 } catch (Exception ex) {
@@ -132,22 +132,15 @@ public class NameStandardizerTaskInitializer extends ScheduledTaskInitializer {
                 }
             });
         } catch (Exception ee) {
-            log.error("error generating standard names: ", ee);
+            log.error("Error generating standard names: ", ee);
             l.message("ERROR:" + ee.getMessage());
             throw new RuntimeException(ee);
         }
 
-        l.message("Shutting down executor service");
-        executor.shutdown();
-        try {
-            executor.awaitTermination(1, TimeUnit.DAYS);
-        } catch (InterruptedException e) {
-            //should never happen
-            log.error("Interrupted exception!");
-        }
-        l.message("Task finished");
+        long end = TimeUtil.getCurrentTimeMillis();
         l.complete();
-        //listen.doneProcess();
+        l.message("Full time to execute, (End-Start): "+ (end-start));
+        log.info("Full time to execute, (End-Start): "+ (end-start));
     }
 
     @Override
@@ -170,37 +163,41 @@ public class NameStandardizerTaskInitializer extends ScheduledTaskInitializer {
      * @return File object ready for report output
      */
     private File getOutputFile() {
+        // It will use supplied path if outputPath is specified in the task config.
+        // On single Tomcat, it may be best to use a full path for the supplied path.
         return FilePathParserUtils.getFileParserBuilder()
-                .suppliedFilePath(outputPath)
-                .defaultFilePath("reports/" + name + "-%DATE% %TIME%.txt")
-                .dateFormatter(formatter)
-                .timeFormatter(formatterTime)
-                .build()
-                .getFile();
+        .suppliedFilePath(outputPath)
+        .defaultFilePath("reports/" + name + "-%DATE% %TIME%.txt")
+        .dateFormatter(formatter)
+        .timeFormatter(formatterTime)
+        .build()
+        .getFile();
     }
 
     private boolean standardizeName(Name name, PrintStream printStream){
-    	initIfNeeded();
+        initIfNeeded();
         //log.trace("starting in standardizeName");
         Boolean nameChanged = false;
         try {
-                log.trace("in StandardNameValidator, Name '{}'; stand.  name: '{}'", name.getName(), name.stdName);
+            log.trace("in StandardNameValidator, Name '{}'; stand.  name: '{}'", name.getName(), name.stdName);
 
-                String prevStdName = name.stdName;
-                String newlyStdName =this.stdNameStandardizer.standardize(name.getName()).getResult();
-                if (!newlyStdName.equals(prevStdName)) {
-                    printStream.format( "%s\t%s\tExisting standardized name for %s, '%s' differs from automatically standardized name: '%s'\n",
-                            prevStdName, newlyStdName, name.getName(), prevStdName, newlyStdName);
-                }
-                if (forceRecalculationOfAll || (name.stdName != null && name.stdName.equals(regenerateNameValue))) {
-                    name.stdName = null;
-                }
+            String prevStdName = name.stdName;
+            String newlyStdName =this.stdNameStandardizer.standardize(name.getName()).getResult();
+            if (!newlyStdName.equals(prevStdName)) {
+                printStream.format( "%s\t%s\tExisting standardized name for %s, '%s' differs from automatically standardized name: '%s'\n",
+                prevStdName, newlyStdName, name.getName(), prevStdName, newlyStdName);
+            }
+            // If told explicitly to regenerate all names, or if stdName not null but effectively null, then null out the stdName field
+            // to signal that the name should be regenerated.
+            if (forceRecalculationOfAll || (name.stdName != null && name.stdName.equals(regenerateNameValue))) {
+                name.stdName = null;
+            }
 
-                if (name.stdName == null || name.stdName.length()==0) {
-                    name.stdName = newlyStdName;
-                    log.debug("set (previously null) stdName to " + name.stdName);
-                    nameChanged= true;
-                }
+            if (name.stdName == null || name.stdName.length()==0) {
+                name.stdName = newlyStdName;
+                log.debug("set (previously null) stdName to " + name.stdName);
+                nameChanged= true;
+            }
 
         } catch (Exception ex) {
             log.error("Error processing names");
@@ -211,54 +208,104 @@ public class NameStandardizerTaskInitializer extends ScheduledTaskInitializer {
 
     private void processNames(SchedulerPlugin.TaskListener l, PrintStream printStream){
         log.trace("starting in processNames");
-
         List<String> nameIds= nameRepository.getAllUuids();
         log.trace("total names: {}", nameIds.size());
-        int soFar =0;
-        for (String nameId: nameIds) {
-            soFar++;
+        log.info("Running NameStandardizer Task with disabledHistory=%, disabledHooks=%s", disabledHistory, disabledHooks);
+        EntityManager em = StaticContextAccessor.getEntityManagerFor(Name.class);
+        EntityPersistAdapter epa = StaticContextAccessor.getBean(EntityPersistAdapter.class);
+        EntityUtils.EntityInfo<Name> nei= EntityUtils.getEntityInfoFor(Name.class);
+        AtomicInteger soFar = new AtomicInteger(0);
+        nameIds.parallelStream().forEach(nameId->{
+            soFar.incrementAndGet();
             log.trace("going to fetch name with ID {}", nameId);
+            /*
+            This way of getting the nameUuid did not work after moving the code block
             UUID nameUuid= UUID.fromString(nameId);
             Optional<Name> nameOpt = nameRepository.findById(nameUuid);
+            */
+            EntityUtils.Key key = EntityUtils.Key.ofStringId(nei, nameId);
+            Optional<Name> nameOpt = EntityFetcher.of(key).getIfPossible().map(n -> (Name) n);
             if( !nameOpt.isPresent()){
                 log.info("No name found with ID {}", nameId);
-                continue;
+                return;
             }
             Name name = nameOpt.get();
             log.trace("processing name with ID {}", name.uuid.toString());
+            // If this method (standardizeName) ends up mutating the name, then we do the following steps
             if(standardizeName(name, printStream) ) {
                 try {
                     log.trace("resaving name {}", name.getName());
-                    //name.forceUpdate();
                     TransactionTemplate tx = new TransactionTemplate(platformTransactionManager);
-                    //log.trace("got tx " + tx);
-                    tx.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRES_NEW);
                     tx.setReadOnly(false);
-                    tx.executeWithoutResult(c-> {
-                        log.trace("before saveAndFlush");
-                        //log.trace("key: " + EntityUtils.EntityWrapper.of(name).getKey());
-                        //log.trace("json: " + EntityUtils.EntityWrapper.of(name).toInternalJson());
-                        //hack to make sure name persists
-                        Name name2 = StaticContextAccessor.getEntityManagerFor(Name.class).merge(name);
-                        //log.trace("name2 dirtiness: " + name2.isDirty());
-                        name2.forceUpdate();
-                        //log.trace("name2 dirtiness after update: " + name2.isDirty());
-                        nameRepository.saveAndFlush(name2);
-                        //log.trace("finished saveAndFlush");
-                    });
 
-                    log.trace("saved name {}", name.getName());
+                    tx.executeWithoutResult(c-> {
+                        if (disabledHistory && disabledHooks) {
+                            System.out.println("Case 1 both disabled");
+                            epa.runWithDisabledHistory(()-> {
+                                System.out.println("Before runWithDisabledHooks, the Thread name is " + Thread.currentThread().getName());
+                                GsrsEntityProcessorListener b =  StaticContextAccessor.getBean(GsrsEntityProcessorListener.class);
+                                System.out.println("The processorId is: " + b.getProcessorId());
+                                b.setTempString( Thread.currentThread().getName());
+                                b.runWithDisabledHooks(()->{
+                                    System.out.println("After runWithDisabledHooks, the Thread name is " + Thread.currentThread().getName());
+                                    System.out.println("The processorId is: " + b.getProcessorId());
+                                    saveWork(em, name);
+                                }); // HOOKS
+                            }); // HISTORY
+                        } else if (disabledHistory) {
+                            System.out.println("Case 2 only disabled history");
+                            epa.runWithDisabledHistory(()-> {
+                                System.out.println("Before runWithDisabledHooks, the Thread name is " + Thread.currentThread().getName());
+                                GsrsEntityProcessorListener b =  StaticContextAccessor.getBean(GsrsEntityProcessorListener.class);
+                                System.out.println("The processorId is: " + b.getProcessorId());
+                                b.setTempString( Thread.currentThread().getName());
+                                saveWork(em, name);
+                            }); // HISTORY
+                        } else if (disabledHooks) {
+                            System.out.println("Case 3 only disabled hooks");
+                            System.out.println("Before runWithDisabledHooks, the Thread name is " + Thread.currentThread().getName());
+                            GsrsEntityProcessorListener b =  StaticContextAccessor.getBean(GsrsEntityProcessorListener.class);
+                            System.out.println("The processorId is: " + b.getProcessorId());
+                            b.setTempString( Thread.currentThread().getName());
+                            b.runWithDisabledHooks(()->{
+                                System.out.println("After runWithDisabledHooks, the Thread name is " + Thread.currentThread().getName());
+                                System.out.println("The processorId is: " + b.getProcessorId());
+                                saveWork(em, name);
+                            }); // HOOKS
+                        } else {
+                            System.out.println("Case 4 neither disabled");
+                            saveWork(em, name);
+                        }
+                    }); // executeWithoutResult
                 } catch (Exception ex) {
                     log.error("Error during save: {}", ex.getMessage());
                 }
             }
-            l.message(String.format("Processed %d of %d names", soFar, nameIds.size()));
+            l.message(String.format("Processed %d of %d names", soFar.get(), nameIds.size()));
+            log.info(String.format("Processed %d of %d names", soFar.get(), nameIds.size()));
+        });
+    }
+
+    private void saveWork(EntityManager em, Name name) {
+        try {
+            log.trace("before saveAndFlush");
+            // log.trace("key: " + EntityUtils.EntityWrapper.of(name).getKey());
+            // log.trace("json: " + EntityUtils.EntityWrapper.of(name).toInternalJson());
+            // hack to make sure name persists
+            Name name2 = em.merge(name);
+            // log.trace("name2 dirtiness: {}" , (log.isTraceEnabled()) ? name2.isDirty(): "");
+            name2.forceUpdate();
+            log.trace("name2 dirtiness after update: {}", (log.isTraceEnabled()) ? name2.isDirty() : "");
+            nameRepository.saveAndFlush(name2);
+            // log.trace("finished saveAndFlush");
+        } catch (Exception ex) {
+            log.error("Error during save while executing transaction: {}", ex.getMessage());
         }
     }
 
     private PrintStream makePrintStream(File writeFile) throws IOException {
         return new PrintStream(
-                new BufferedOutputStream(new FileOutputStream(writeFile)),
-                false, STANDARD_FILE_ENCODING);
+        new BufferedOutputStream(new FileOutputStream(writeFile)),
+        false, STANDARD_FILE_ENCODING);
     }
 }
