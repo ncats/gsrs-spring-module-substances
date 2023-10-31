0385dab8dbe60c4f9259d51c1af7776bae403dfb
Tyler Peryea <tylerperyea@gmail.com>
2023-10-31 17:34:51 -0400

    updated renderer jar

--- installExtraJars.sh
+++ installExtraJars.sh
@@ -1,4 +1,4 @@
 ./mvnw install:install-file -Dfile=extraJars/applications-api-3.1-SNAPSHOT.jar
 ./mvnw install:install-file -Dfile=extraJars/products-api-3.1-SNAPSHOT.jar
 ./mvnw install:install-file -Dfile=extraJars/clinical-trials-api-3.1-SNAPSHOT.jar
-./mvnw install:install-file -Dfile=extraJars/molwitch-renderer-1.0.13-SNAPSHOT.jar
+./mvnw install:install-file -Dfile=extraJars/molwitch-renderer-1.0.14-SNAPSHOT.jar
