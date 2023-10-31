7f2a3e540a2ab5c856f304e740cc1c9f8b8b86a1
Tyler Peryea <tylerperyea@gmail.com>
2023-10-31 15:26:01 -0400

    updated cdk renderer

--- gsrs-module-substances-core/src/main/java/gsrs/module/substance/controllers/SubstanceController.java
+++ gsrs-module-substances-core/src/main/java/gsrs/module/substance/controllers/SubstanceController.java
@@ -1405,7 +1405,6 @@ public class SubstanceController extends EtagLegacySearchEntityController<Substa
                 rendererOptions.changeSettings(newDisplay);
             }
 
-
             //TODO: This would be nice to get back eventually, for standardization:
             //chem.reduceMultiples();
 
@@ -1532,6 +1531,7 @@ public class SubstanceController extends EtagLegacySearchEntityController<Substa
             boolean hasRgroups=hasRGroups(c);
             int rgroupColor=1;
 
+            
             if(hasRgroups){
                 if(fuse){
                     compColor=colorChemicalComponents(c);
@@ -1616,6 +1616,8 @@ public class SubstanceController extends EtagLegacySearchEntityController<Substa
 //			}
 //		}
         boolean r=false;
+
+        
         for(Atom ca:c.getAtoms()){
             if(ca.getRGroupIndex().isPresent()){
                 r= true;
@@ -1625,6 +1627,9 @@ public class SubstanceController extends EtagLegacySearchEntityController<Substa
                 String alias = ca.getAlias().orElse("");
                 if(alias.startsWith("_R")){
                     ca.setRGroup(Integer.parseInt(alias.replace("_R", "")));
+                    r= true;
+                }else if(alias.startsWith("R")){
+                    ca.setRGroup(Integer.parseInt(alias.replace("R", "")));
                     r= true;
                 }
             }
