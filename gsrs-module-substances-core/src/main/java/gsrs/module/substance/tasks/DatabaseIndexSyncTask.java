dbfb98e3e9c64646ef9e1d1e063d1e82f7b71555
Lihui Hu <lihui.hu@nih.gov>
2024-05-09 14:11:50 -0400

    make it work only for substances

--- gsrs-module-substances-core/src/main/java/gsrs/module/substance/tasks/DatabaseIndexSyncTask.java
+++ gsrs-module-substances-core/src/main/java/gsrs/module/substance/tasks/DatabaseIndexSyncTask.java
@@ -25,11 +25,9 @@ public class DatabaseIndexSyncTask {
 			
 	@Value("${database.index.sync.scheduler.entities:}")
 	private String entityString;	
-	@Value("${database.index.sync.scheduler.cron}")
-	private String cron;
+
 	
-	private final Set<String> allowedEntities = Util.toSet("Code","ControlledVocabulary","Name", "Reference","Substance",
-			"Product","Application","ApplicationAll","ClinicalTrialUS","ClinicalTrialEurope");
+	private final Set<String> supportedEntities = Util.toSet("Code","ControlledVocabulary","Name", "Reference","Substance");
 				
 	@Scheduled(cron= "${database.index.sync.scheduler.cron}")	
 	public void runSyncTask() {
@@ -40,25 +38,23 @@ public class DatabaseIndexSyncTask {
 		}
 		
 		Set<String> syncEntities = Stream.of(entityString.split(",")).map(entity->entity.trim()).collect(Collectors.toSet());
-		syncEntities.retainAll(allowedEntities);
+		syncEntities.retainAll(supportedEntities);
 		if(syncEntities.size()==0) {
 			log.info("No allowed entities defined for the database and index sync scheduler.");
 			return;
 		}
 				
 		for(String entity: syncEntities) {
-			
-			System.out.println("Sync entity class: " + entity);
+						
 			String entityClassName = generateEntityClassName(entity);
+			log.info("Entity class: " + entityClassName);
 			
 			if(entityClassName.isEmpty()) {
 				log.warn("Illegal entity class: " + entity + " in database and index sync scheduler.");
 				continue;
 			}
 			
-			Class<?> entityClass;			
-			log.info("Entity class: " + entityClassName);
-				
+			Class<?> entityClass;				
 			try {
 				entityClass = Class.forName(entityClassName);
 			} catch (ClassNotFoundException e) {
@@ -82,38 +78,11 @@ public class DatabaseIndexSyncTask {
 				log.error("Error in database and index sync scheduler: " + entityClassName);
 				e.printStackTrace();
 				continue;
-			}					
-
+			}
 		}
 	}
 	
 	private String generateEntityClassName(String entity) {
-		final String coreEntitiesPrefix = "ix.ginas.models.v1.";
-		final String applicationClassName = "gov.hhs.gsrs.application.application.models.Application";
-		final String applicationAllClassName = "gov.hhs.gsrs.application.applicationall.models.ApplicationAll";
-		final String productClassName = "gov.hhs.gsrs.products.product.models.Product";
-		final String clinicalTrialUSClassName = "gov.hhs.gsrs.clinicaltrial.us.models.ClinicalTrialUS";
-		final String clinicalTrialEuropeClassName = "gov.hhs.gsrs.clinicaltrial.europe.models.ClinicalTrialEurope";		
-		
-		switch(entity) {
-		case "Code":
-		case "ControlledVocabulary":
-		case "Name":
-		case "Reference":
-		case "Substance":
-			return coreEntitiesPrefix + entity;
-		case "Application":
-			return applicationClassName;  
-		case "ApplicationAll":
-			return applicationAllClassName;
-		case "Product":
-			return productClassName;
-		case "ClinicalTrialUS":
-			return clinicalTrialUSClassName;
-		case "ClinicalTrialEurope":
-			return clinicalTrialEuropeClassName;
-		default: 
-			return "";
-		}		
+		return "ix.ginas.models.v1." + entity;		
 	}
 }
