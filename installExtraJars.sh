ce50fa8fe38edc07fc965b0783b0b0cdd88a8bff
Mitch Miller <mitch.miller@thinkscience.us>
2024-06-04 08:55:31 -0400

    adding latest molwitch-renderer to extraJars

--- installExtraJars.sh
+++ installExtraJars.sh
@@ -1,3 +1,4 @@
 ./mvnw install:install-file -Dfile=extraJars/applications-api-3.1-SNAPSHOT.jar
 ./mvnw install:install-file -Dfile=extraJars/products-api-3.1.1-SNAPSHOT.jar
 ./mvnw install:install-file -Dfile=extraJars/clinical-trials-api-3.1.1-SNAPSHOT.jar
+./mvnw install:install-file -Dfile=extraJars/molwitch-renderer-1.0.17.jar
