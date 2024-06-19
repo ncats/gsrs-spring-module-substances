774084baa07cba92609c866b4dfc723a22f253c8
alx652 <welscha2@nih.gov>
2024-06-19 14:56:57 -0400

    updates for 3.1.0.6

--- installExtraJars.cmd
+++ installExtraJars.cmd
@@ -1,4 +1,3 @@
-mvnw.cmd install:install-file -Dfile=extraJars/applications-api-3.1-SNAPSHOT.jar
-mvnw.cmd install:install-file -Dfile=extraJars/products-api-3.1-SNAPSHOT.jar
-mvnw.cmd install:install-file -Dfile=extraJars/clinical-trials-api-3.1-SNAPSHOT.jar
-
+mvnw.cmd install:install-file -Dfile=extraJars/applications-api-3.1.0.6.jar
+mvnw.cmd install:install-file -Dfile=extraJars/products-api-3.1.0.6.jar
+mvnw.cmd install:install-file -Dfile=extraJars/clinical-trials-api-3.1.0.6.jar
