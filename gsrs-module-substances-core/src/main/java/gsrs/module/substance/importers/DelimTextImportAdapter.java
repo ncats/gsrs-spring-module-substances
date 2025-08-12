a27765d397d2fa9c510e17e08cf02109175cf5b0
Mitch Miller <mitch.miller@thinkscience.us>
2025-08-12 18:52:18 -0400

    minor tweak

--- gsrs-module-substances-core/src/main/java/gsrs/module/substance/importers/DelimTextImportAdapter.java
+++ gsrs-module-substances-core/src/main/java/gsrs/module/substance/importers/DelimTextImportAdapter.java
@@ -93,7 +93,7 @@ public class DelimTextImportAdapter implements ImportAdapter<Substance> {
                         r.getProperties().forEach(p-> log.trace("   property: {}; value: {}", p, r.getProperty(p).isPresent() ? r.getProperty(p).get() : "absent"));
                     } )
                     .filter(r->r.getProperties() != null && r.getProperties().size() >0
-                            /*&& r.getProperties().stream().anyMatch(p-> r.getProperty(p).isPresent() && r.getProperty(p).get().length()>0)*/)
+                            && r.getProperties().stream().anyMatch(p-> r.getProperty(p).isPresent() && r.getProperty(p).get() != null && r.getProperty(p).get().length()>0))
                     .map(r->{
                         log.trace("in map that follows filter");
                         AbstractSubstanceBuilder s;
