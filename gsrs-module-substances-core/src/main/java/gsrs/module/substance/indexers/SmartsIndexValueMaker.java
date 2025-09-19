05b87344af7ac485bda0a4791288d64f03b5415f
Mitch Miller <mitch.miller@thinkscience.us>
2025-09-18 20:17:50 -0400

    new clean branch for SMARTS IVM

---
+++ gsrs-module-substances-core/src/main/java/gsrs/module/substance/indexers/SmartsIndexValueMaker.java
@@ -0,0 +1,119 @@
+package gsrs.module.substance.indexers;
+
+import gov.nih.ncats.common.stream.StreamUtil;
+import gov.nih.ncats.molwitch.search.MolSearcher;
+import gov.nih.ncats.molwitch.search.MolSearcherFactory;
+import ix.core.search.text.IndexValueMaker;
+import ix.core.search.text.IndexableValue;
+import ix.ginas.models.v1.ChemicalSubstance;
+import ix.ginas.models.v1.Substance;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Consumer;
+
+@Slf4j
+public class SmartsIndexValueMaker implements IndexValueMaker<Substance> {
+
+    public static final String FACET_NAME_FULL ="Structure Facet";
+
+    private final String DEFAULT_CONFIG_DATA = "nitro_[$([NX3](=O)=O),$([NX3+](=O)[O-])][!#8]€nitroso_[NX2]=[OX1]";
+
+    List<SmartsIndexable> indexables = new ArrayList<>();
+
+    private boolean setupComplete=false;
+
+    public final static String NAME_TO_VALUE_DELIM = "₠";
+
+    //private String SET_TO_SET_DELIM = "¥";
+
+    //private String VALUE_TO_VALUE_DELIM = "€";
+
+    private void completeSetup(){
+        log.trace("completeSetup indexables size: {}", indexables.size());
+
+        if( this.indexables != null && this.indexables.size() > 0) {
+            log.info("using provided configuration");
+            return;
+        }
+
+        indexables = new ArrayList<>();
+        String usableConfig = DEFAULT_CONFIG_DATA;
+        log.trace("using default config {}", usableConfig);
+        String[] tokens = usableConfig.split("\\€");
+        for(String token : tokens) {
+            String[] parts = token.split("\\_");
+            Map<String, String> values = new LinkedHashMap<>();
+            AtomicInteger item = new AtomicInteger(0);
+            String[] innerParts = parts[1].split(NAME_TO_VALUE_DELIM);
+            for(String part :innerParts) {
+                values.put( Integer.toString( item.getAndIncrement()), part);
+            }
+            SmartsIndexable indexable = new SmartsIndexable(parts[0], values);
+            log.trace("indexable with {} and {}", parts[0], parts[1]);
+            indexables.add(indexable);
+        }
+    }
+
+    @Override
+    public Class<Substance> getIndexedEntityClass() {
+        return Substance.class;
+    }
+
+    @Override
+    public void createIndexableValues(Substance substance, Consumer<IndexableValue> consumer) {
+        if( !(substance instanceof ChemicalSubstance)) {
+            return;
+        }
+
+        if(!setupComplete) {
+            completeSetup();
+            setupComplete=true;
+        }
+            ChemicalSubstance chemical = (ChemicalSubstance) substance;
+            indexables.forEach(i->{
+                log.trace("Looking for chemical matches using smarts with name {} with {} SMARTS patterns to match", i.getName(), i.getSMARTSList().size());
+                boolean foundMatchWithCurrentIdea =false;
+                for(int item = 0; item< i.getSMARTSList().size() && !foundMatchWithCurrentIdea; item++) {
+                    String smarts = i.getSMARTSList().get(item);
+                    Optional<MolSearcher> optSearcher= MolSearcherFactory.create(smarts);
+                    if( optSearcher.isPresent()) {
+                        MolSearcher searcher= optSearcher.get();
+                        Optional<int[]> matches= searcher.search(chemical.toChemical());
+                        if( matches.isPresent() && matches.get().length>0) {
+                            foundMatchWithCurrentIdea=true;
+                        }
+                    }
+                }
+                if( foundMatchWithCurrentIdea) {
+                    log.trace("chemical matches smarts with name {}", i.getName());
+                    consumer.accept(IndexableValue.simpleFacetStringValue(FACET_NAME_FULL, i.getName()));
+
+                }
+                log.trace("after indexing");
+            });
+    }
+
+    public List<SmartsIndexable> getIndexables() {
+        return indexables;
+    }
+
+    public void setIndexables(List<SmartsIndexable> indexables) {
+        this.indexables = indexables;
+    }
+
+    public void setRawIndexables( LinkedHashMap<Integer, Map<String, Object>> newIndexables){
+        log.trace("starting in setRawIndexables");
+        this.indexables.clear();
+        for (Map<String, Object> rawIndexable: newIndexables.values()) {
+            String patternName = (String) rawIndexable.get("indexableName");
+            Map<String, String> smartsList = (Map<String, String>) rawIndexable.get("smarts");
+            SmartsIndexable indexable = new SmartsIndexable(patternName, smartsList);
+            if (indexable.isValid()) {
+                this.indexables.add(indexable);
+            }
+        }
+        log.trace("indexables has {} items", indexables.size());
+    }
+}
