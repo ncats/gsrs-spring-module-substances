ce458671e46955f9e90357e82e7974f3373af5af
Mitch Miller <mitch.miller@thinkscience.us>
2025-11-03 21:48:35 -0500

    new validator for controlled vocabularies - make sure there are no duplicates:q

---
+++ gsrs-module-substances-core/src/main/java/ix/ginas/utils/validation/validators/CVValidator.java
@@ -0,0 +1,46 @@
+package ix.ginas.utils.validation.validators;
+
+import ix.core.validator.GinasProcessingMessage;
+import ix.core.validator.ValidatorCallback;
+import ix.ginas.models.v1.ControlledVocabulary;
+import ix.ginas.utils.validation.AbstractValidatorPlugin;
+import jdk.jpackage.internal.Log;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Slf4j
+public class CVValidator extends AbstractValidatorPlugin<ControlledVocabulary> {
+    @Override
+    public void validate(ControlledVocabulary objnew, ControlledVocabulary objold, ValidatorCallback callback) {
+        //look for duplicates
+        Map<String, Long> occurrencesOfTerms= objnew.terms.stream()
+                .map(t-> t.value)
+                .collect(Collectors.groupingBy(s-> s, Collectors.counting()));
+        Map<String, Long> occurrencesOfDisplays= objnew.terms.stream()
+                .map(t-> t.display)
+                .collect(Collectors.groupingBy(s-> s, Collectors.counting()));
+        List<String> duplicateTerms = occurrencesOfTerms.entrySet().stream()
+                .filter(en->en.getValue() >1)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
+        List<String> duplicateDisplays = occurrencesOfDisplays.entrySet().stream()
+                .filter(en->en.getValue() > 1)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
+        if(!duplicateTerms.isEmpty()) {
+            callback.addMessage(GinasProcessingMessage.ERROR_MESSAGE(
+                    "These terms occur more than once: %s", String.join(", ", duplicateTerms)));
+            callback.setInvalid();
+            log.info("found duplicate vocabulary terms: {}", String.join(", ", duplicateTerms));
+        }
+        if(!duplicateDisplays.isEmpty()) {
+            callback.addMessage(GinasProcessingMessage.ERROR_MESSAGE(
+                    "The display values occur more than once: %s", String.join(", ", duplicateDisplays)));
+            callback.setInvalid();
+            log.info("found duplicate vocabulary displays: {}", String.join(", ", duplicateDisplays));
+        }
+    }
+}
