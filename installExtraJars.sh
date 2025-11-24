43fa9d383ce2ef2838aa5f506c8aaa4b471d1066
alx652 <welscha2@nih.gov>
2025-11-24 14:27:11 -0500

    save

--- installExtraJars.sh
+++ installExtraJars.sh
@@ -17,9 +17,9 @@ set -x
 
 # ./mvnw install:install-file -Dfile=extraJars/Featureize-Nitrosamines-0.0.4-SNAPSHOT.jar
 
-mvn install:install-file -Dfile=extraJars/gsrs-spring-module-drug-applications-3.1.3-SNAPSHOT-pom.xml -DpomFile=extraJars/gsrs-spring-module-drug-applications-3.1.3-SNAPSHOT-pom.xml
-mvn install:install-file -Dfile=extraJars/gsrs-spring-module-clinical-trials-3.1.3-SNAPSHOT-pom.xml -DpomFile=extraJars/gsrs-spring-module-clinical-trials-3.1.3-SNAPSHOT-pom.xml
-mvn install:install-file -Dfile=extraJars/gsrs-spring-module-drug-products-3.1.3-SNAPSHOT-pom.xml -DpomFile=extraJars/gsrs-spring-module-drug-products-3.1.3-SNAPSHOT-pom.xml 
+# mvn install:install-file -Dfile=extraJars/gsrs-spring-module-drug-applications-3.1.3-SNAPSHOT-pom.xml -DpomFile=extraJars/gsrs-spring-module-drug-applications-3.1.3-SNAPSHOT-pom.xml
+# mvn install:install-file -Dfile=extraJars/gsrs-spring-module-clinical-trials-3.1.3-SNAPSHOT-pom.xml -DpomFile=extraJars/gsrs-spring-module-clinical-trials-3.1.3-SNAPSHOT-pom.xml
+# mvn install:install-file -Dfile=extraJars/gsrs-spring-module-drug-products-3.1.3-SNAPSHOT-pom.xml -DpomFile=extraJars/gsrs-spring-module-drug-products-3.1.3-SNAPSHOT-pom.xml 
 
 mvn install:install-file -Dfile=extraJars/applications-api-3.1.3-SNAPSHOT.jar -DgroupId=gov.nih.ncats -DartifactId=applications-api -Dversion=3.1.3-SNAPSHOT -Dpackaging=jar
 mvn install:install-file -Dfile=extraJars/clinical-trials-api-3.1.3-SNAPSHOT.jar -DgroupId=gov.nih.ncats -DartifactId=clinical-trials-api -Dversion=3.1.3-SNAPSHOT -Dpackaging=jar
