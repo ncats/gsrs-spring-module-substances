a6df8f8cc0786da96c94203a5b60b52b0272df9d
alx652 <welscha2@nih.gov>
2026-03-27 20:02:43 -0400

    add substance definition

--- gsrs-module-substances-core/src/main/java/gsrs/module/substance/misc/emasmsfhir/EmaSmsFhirController.java
+++ gsrs-module-substances-core/src/main/java/gsrs/module/substance/misc/emasmsfhir/EmaSmsFhirController.java
@@ -1,9 +1,9 @@
 package gsrs.module.substance.misc.emasmsfhir;
 
 import ca.uhn.fhir.context.FhirContext;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import gsrs.module.substance.SubstanceEntityService;
 import ix.ginas.models.v1.Substance;
+import org.hl7.fhir.r5.model.SubstanceDefinition;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.http.HttpHeaders;
@@ -13,38 +13,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Optional;
 
 @RestController
 public class EmaSmsFhirController {
-    private static String DEFAULT_NAME_SOURCE = "FDA SUBSTANCE REGISTRATION SYSTEM";
-    private static String DEFAULT_LANGUAGE = "en";
-    private static String SodiumChlorideUuid = "306d24b9-a6b8-4091-8024-02f9ec24b705";
+
+    private static final String SodiumChlorideUuid = "306d24b9-a6b8-4091-8024-02f9ec24b705";
     // http://localhost:8080/api/v1/substances/306d24b9-a6b8-4091-8024-02f9ec24b705/@emaSmsRecord
+    // http://localhost:8080/api/v1/substances/306d24b9-a6b8-4091-8024-02f9ec24b705/@emaSmsSubstanceDefinition
 
-    private static String sodiumGlutonateUuid = "90e9191d-1a81-4a53-b7ee-560bf9e68109";
+    private static final String sodiumGlutonateUuid = "90e9191d-1a81-4a53-b7ee-560bf9e68109";
     // http://localhost:8080/api/v1/substances/90e9191d-1a81-4a53-b7ee-560bf9e68109/@emaSmsRecord
-
-    private static Map<String, String> FIELD_MAP;
-
-    static {
-        FIELD_MAP = new HashMap<>();
-        FIELD_MAP.put("smsId", "SMS_ID");
-        FIELD_MAP.put("substanceName", "Substance_Name");
-        FIELD_MAP.put("isPreferredName", "Is_Preferred_Name");
-        FIELD_MAP.put("language", "Language");
-        FIELD_MAP.put("substanceType", "Substance_Type");
-        FIELD_MAP.put("evCode", "EV_Code");
-        FIELD_MAP.put("unii", "UNII");
-        FIELD_MAP.put("innNumber", "INN_Number");
-        FIELD_MAP.put("ecListNumber", "EC_List/Number");
-    }
-
-
-    ObjectMapper mapper = new ObjectMapper();
+    // http://localhost:8080/api/v1/substances/90e9191d-1a81-4a53-b7ee-560bf9e68109/@emaSmsSubstanceDefinition
 
     @Autowired
     private ApplicationContext applicationContext;
@@ -72,8 +52,24 @@ public class EmaSmsFhirController {
         }
     }
 
-
-
-
+    @GetMapping(value = "/api/v1/substances/{id}/@emaSmsSubstanceDefinition")
+    public ResponseEntity<?> makeEmaSmsSubstanceDefinition(@PathVariable("id") String id) {
+        String jsonEncoded;
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        try {
+            Optional<Substance> gsrsSubstance = substanceEntityService.flexLookup(id);
+            if (!gsrsSubstance.isPresent()) {
+                return new ResponseEntity<>("{\"message\": \"Substance entity for FHIR resource not found\"}", headers, HttpStatus.NOT_FOUND);
+            }
+            FhirContext ctx = FhirContext.forR5();
+            EmaSmsSubstanceDefinitionFhirMapper emaSmsSubstanceDefinitionFhirMapper = new EmaSmsSubstanceDefinitionFhirMapper();
+            SubstanceDefinition substanceDefinition = emaSmsSubstanceDefinitionFhirMapper.generateEmaSmsSubstanceDefinitionFromSubstance(gsrsSubstance.get());
+            jsonEncoded = ctx.newJsonParser().setPrettyPrint(true).encodeResourceToString(substanceDefinition);
+            return new ResponseEntity<>(jsonEncoded, headers, HttpStatus.FOUND);
+        } catch (Exception e) {
+            return new ResponseEntity<>("{\"message\": \"Internal error generating FHIR resource\"}", headers, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
 
---
+++ gsrs-module-substances-core/src/main/java/gsrs/module/substance/misc/emasmsfhir/EmaSmsSubstanceDefinitionFhirMapper.java
@@ -0,0 +1,191 @@
+package gsrs.module.substance.misc.emasmsfhir;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import ix.ginas.models.v1.Name;
+import ix.ginas.models.v1.Substance;
+import lombok.Data;
+import org.hl7.fhir.r5.model.*;
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Data
+public class EmaSmsSubstanceDefinitionFhirMapper {
+    private static final String DEFAULT_NAME_SOURCE = "FDA SUBSTANCE REGISTRATION SYSTEM";
+    private static final String DEFAULT_LANGUAGE = "en";
+    private static final String SUBTANCE_TYPE_SYSTEM_URL = "http://example.europa.eu/fhir/SubstanceType";
+    private static final String CODE_SYSTEM_URL="http://example.europa.eu/fhir/Substance";
+
+    private static final String UNII_SYSTEM_URL="https://example.europa.eu/fhir/Substance#---UNII";
+    private static final String EV_SYSTEM_URL="https://example.europa.eu/fhir/Substance#---EV";
+    private static final String INN_SYSTEM_URL="https://example.europa.eu/fhir/Substance#---INN";
+    private static final String ECHA_SYSTEM_URL="https://example.europa.eu/fhir/Substance#----ECHA";
+    private static final String STATUS_SYSTEM_URL="https://example.europa.eu/fhir/Substance#--status";
+
+    private static final String GSRS_SUBSTANCE_EXTENSION_URL="https://gsrs.ncats.nih.gov/api/v1/substances";
+
+    private static final Map<String, Map<String, String>> SUBSTANCE_CLASS_MAP;
+    private static final Locale[] locales = Locale.getAvailableLocales();
+    // delete private static Map<String, String> LANGUAGE_MAP;
+
+    static  {
+
+        SUBSTANCE_CLASS_MAP = new HashMap<>();
+
+        Map chemicalMap = new HashMap<>();
+        chemicalMap.put("display", "Chemical");
+        chemicalMap.put("code", "100000075670");
+        chemicalMap.put("system", "http://example.europa.eu/fhir/SubstanceType");
+        SUBSTANCE_CLASS_MAP.put("chemical", chemicalMap);
+
+        Map conceptMap = new HashMap<>();
+        conceptMap.put("display", "Concept");
+        conceptMap.put("code", "?");
+        conceptMap.put("system", "http://example.europa.eu/fhir/SubstanceType");
+        SUBSTANCE_CLASS_MAP.put("concept", conceptMap);
+    }
+
+    private boolean includeGsrsSubstanceExtension = false;
+
+    public SubstanceDefinition generateEmaSmsSubstanceDefinitionFromSubstance(Substance substance) {
+
+
+
+        SubstanceDefinition substanceDefinition = new SubstanceDefinition();
+
+        Optional<Name> optionalDisplayName = substance.getDisplayName();
+
+        String substanceClassName = substance.substanceClass.name();
+
+        substanceDefinition.setId("example");
+
+        // Name, Do we want all Names?
+        optionalDisplayName.ifPresent(displayName -> substanceDefinition.addName(makeSubstanceDefinitionNameComponent(displayName)));
+
+        // classification
+        substanceDefinition.getClassification()
+            .add(new CodeableConcept()
+            .addCoding(new Coding()
+                .setCode(querySubstanceClassMap(substanceClassName, "code"))
+                .setSystem(SUBTANCE_TYPE_SYSTEM_URL)
+                .setDisplay("")
+        ));
+
+        // e.g. SUB99611MIG
+        substanceDefinition.addCode(
+            new SubstanceDefinition.SubstanceDefinitionCodeComponent()
+            .setCode(new CodeableConcept()
+            .addCoding(new Coding()
+                .setCode(findCodeByCodeSystem("EVMPD", substance))
+                .setSystem(EV_SYSTEM_URL)
+            )
+        ));
+
+        // e.g. UNII; should we use approval ID instead?
+        substanceDefinition.addCode(
+            new SubstanceDefinition.SubstanceDefinitionCodeComponent()
+            .setCode(new CodeableConcept()
+                .addCoding(new Coding()
+                    .setCode(findCodeByCodeSystem("FDA UNII", substance))
+                    .setSystem(UNII_SYSTEM_URL)
+                    .setDisplay("UNII")
+                )
+        ));
+
+        // INN
+        substanceDefinition.addCode(
+            new SubstanceDefinition.SubstanceDefinitionCodeComponent()
+            .setCode(new CodeableConcept()
+                .addCoding(new Coding()
+                    .setCode(findCodeByCodeSystem("INN", substance))
+                    .setSystem(INN_SYSTEM_URL)
+                )
+        ));
+
+        // ECHA (EC/EINECS)
+        substanceDefinition.addCode(
+            new SubstanceDefinition.SubstanceDefinitionCodeComponent()
+            .setCode(new CodeableConcept()
+                .addCoding(new Coding()
+                    .setCode(findCodeByCodeSystem("ECHA (EC/EINECS)", substance))
+                    .setSystem(ECHA_SYSTEM_URL)
+                )
+            ));
+
+        // Add Extension for GSRS Substance Json
+        if(includeGsrsSubstanceExtension) {
+            substanceDefinition.addExtension(
+                new Extension().setUrl(GSRS_SUBSTANCE_EXTENSION_URL)
+                .setValue(new StringType(gsrsSubstanceToQuotedJson(substance)))
+            );
+        }
+        return substanceDefinition;
+    }
+
+   private SubstanceDefinition.SubstanceDefinitionNameComponent makeSubstanceDefinitionNameComponent(Name name) {
+       return new SubstanceDefinition.SubstanceDefinitionNameComponent()
+           .setName(name.getName())
+           .setLanguage(
+               name.languages.stream().map(kw-> {
+                   return new CodeableConcept()
+                       .addCoding(new Coding()
+                           .setCode(kw.term)
+                           .setSystem("urn:ietf:bcp:47")
+                           // Should we get from CV?
+                           .setDisplay(Locale.forLanguageTag(kw.term).getDisplayLanguage())
+                       );
+               }).collect(Collectors.toList())
+           )
+           // Should we use name.preferred instead?
+           .setPreferred(name.isDisplayName())
+           .setStatus(new CodeableConcept()
+               .addCoding(new Coding()
+                   .setCode("200000005004")
+                   .setSystem(STATUS_SYSTEM_URL)
+                   .setDisplay("Current")
+            )
+       );
+   }
+
+   private String querySubstanceClassMap (String substanceClass, String key) {
+        Map<String, String> map = SUBSTANCE_CLASS_MAP.get(substanceClass);
+        String value = null;
+        if(map!=null) {
+            value = map.get(key);
+        }
+        if(value!=null) {
+            return value;
+        }
+        return "";
+    }
+
+    private String findCodeByCodeSystem (String codeSystem, Substance substance){
+        // Do we need to check if public?
+        boolean publicOnly = false;
+        Optional<String> optionalCode = substance.getCodes()
+                .stream()
+                // .filter(cd -> !(publicOnly && !cd.isPublic()))
+                .filter(cd -> codeSystem.equalsIgnoreCase(cd.codeSystem))
+                .findFirst()
+                .map(cd -> {
+                    if ("PRIMARY".equals(cd.type)) {
+                        return cd.code;
+                    } else {
+                        return cd.code + " [" + cd.type + "]";
+                    }
+                });
+        return optionalCode.orElse("");
+    }
+
+    public String gsrsSubstanceToQuotedJson (Substance substance){
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            return mapper.writeValueAsString(substance);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
+
+
