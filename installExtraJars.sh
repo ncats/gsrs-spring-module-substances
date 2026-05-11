85a33a720727ecbf0e440b4e460fd5a1242c90c1
alx652 <welscha2@nih.gov>
2026-05-11 12:27:48 -0400

    verify branches

--- installExtraJars.sh
+++ installExtraJars.sh
@@ -1,3 +1,4 @@
+# Seems like in 3x, we have to install api Jar's parent pom.xml if we don't install the actual module first.
 ./mvnw install:install-file -Dfile=extraJars/gsrs-spring-module-drug-applications-3.2.5-SNAPSHOT-pom.xml -DpomFile=extraJars/gsrs-spring-module-drug-applications-3.2.5-SNAPSHOT-pom.xml
 ./mvnw install:install-file -Dfile=extraJars/gsrs-spring-module-clinical-trials-3.2.5-SNAPSHOT-pom.xml -DpomFile=extraJars/gsrs-spring-module-clinical-trials-3.2.5-SNAPSHOT-pom.xml
 ./mvnw install:install-file -Dfile=extraJars/gsrs-spring-module-drug-products-3.2.5-SNAPSHOT-pom.xml -DpomFile=extraJars/gsrs-spring-module-drug-products-3.2.5-SNAPSHOT-pom.xml
