15947a24b902eedfb676d6fa3e6fb239c1d25d56
alx652 <welscha2@nih.gov>
2026-05-11 11:52:05 -0400

    add poms to install extra jars

--- installExtraJars.sh
+++ installExtraJars.sh
@@ -1,4 +1,8 @@
-./mvnw install:install-file -Dfile=extraJars/applications-api-3.2.5-SNAPSHOT.jar
+./mvnw install:install-file -Dfile=extraJars/gsrs-spring-module-drug-applications-3.2.5-SNAPSHOT-pom.xml -DpomFile=extraJars/gsrs-spring-module-drug-applications-3.2.5-SNAPSHOT-pom.xml
+./mvnw install:install-file -Dfile=extraJars/gsrs-spring-module-clinical-trials-3.2.5-SNAPSHOT-pom.xml -DpomFile=extraJars/gsrs-spring-module-clinical-trials-3.2.5-SNAPSHOT-pom.xml
+./mvnw install:install-file -Dfile=extraJars/gsrs-spring-module-drug-products-3.2.5-SNAPSHOT-pom.xml -DpomFile=extraJars/gsrs-spring-module-drug-products-3.2.5-SNAPSHOT-pom.xml
+
+./mvnw install:install-file -Dfile=extraJars/applications-api-3.2.5-SNAPSHOT.jar 
 ./mvnw install:install-file -Dfile=extraJars/products-api-3.2.5-SNAPSHOT.jar
 ./mvnw install:install-file -Dfile=extraJars/clinical-trials-api-3.2.5-SNAPSHOT.jar
 ./mvnw install:install-file -Dfile=extraJars/Featureize-Nitrosamines-0.0.4-SNAPSHOT.jar
