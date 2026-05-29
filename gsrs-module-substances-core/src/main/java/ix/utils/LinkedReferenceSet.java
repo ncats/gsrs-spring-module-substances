eb4545703ad1563ae04d87ca073564a41850eeec
alx652 <58182629+alx652@users.noreply.github.com>
2026-05-28 22:27:44 -0400

    Revert "Delete old redundant files (literal reference included in the gsrs starter)  "

---
+++ gsrs-module-substances-core/src/main/java/ix/utils/LinkedReferenceSet.java
@@ -0,0 +1,46 @@
+package ix.utils;
+
+import ix.core.util.UniqueStack;
+
+import java.util.Optional;
+import java.util.stream.Stream;
+
+public class LinkedReferenceSet<K> implements ExecutionStack<K> {
+	UniqueStack<LiteralReference<K>> internalStack = new UniqueStack<LiteralReference<K>>();
+	
+	
+	public boolean contains(K k){
+		return internalStack.contains(LiteralReference.of(k));
+	}
+	
+	
+	@Override
+	public void pushAndPopWith(K obj, Runnable r) {
+		internalStack.pushAndPopWith(LiteralReference.of(obj), r);
+	}
+
+	@Override
+	public K getFirst() {
+		return internalStack.getFirst().get();
+	}
+	
+	@Override
+	public Optional<K> getOptionalFirst() {
+		Optional<LiteralReference<K>> ret =internalStack.getOptionalFirst();
+		if(ret.isPresent()){
+			return Optional.of(ret.get().get());
+		}
+		return Optional.empty();
+	}
+
+	@Override
+	public void setMaxDepth(Integer maxDepth) {
+		internalStack.setMaxDepth(maxDepth);
+	}
+
+	public Stream<K> asStream(){
+		return internalStack.asStream().map(l->l.get());
+	}
+	
+	
+}
