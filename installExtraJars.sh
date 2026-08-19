83c6ae0e43a16e04c23b370c7554a028ffcb4b1e
alx652 <welscha2@nih.gov>
2026-08-18 22:13:44 -0400

    put back parent pom files in extraJars

--- installExtraJars.sh
+++ installExtraJars.sh
@@ -1,7 +1,7 @@
 # Seems like in 3x, we have to install api Jar's parent pom.xml if we don't install the actual module first.
-# ./mvnw install:install-file -Dfile=extraJars/gsrs-spring-module-drug-applications-3.2.5-SNAPSHOT-pom.xml -DpomFile=extraJars/gsrs-spring-module-drug-applications-3.2.5-SNAPSHOT-pom.xml
-# ./mvnw install:install-file -Dfile=extraJars/gsrs-spring-module-clinical-trials-3.2.5-SNAPSHOT-pom.xml -DpomFile=extraJars/gsrs-spring-module-clinical-trials-3.2.5-SNAPSHOT-pom.xml
-# ./mvnw install:install-file -Dfile=extraJars/gsrs-spring-module-drug-products-3.2.5-SNAPSHOT-pom.xml -DpomFile=extraJars/gsrs-spring-module-drug-products-3.2.5-SNAPSHOT-pom.xml
+./mvnw install:install-file -Dfile=extraJars/gsrs-spring-module-drug-applications-3.2.6-SNAPSHOT-pom.xml -DpomFile=extraJars/gsrs-spring-module-drug-applications-3.2.6-SNAPSHOT-pom.xml
+./mvnw install:install-file -Dfile=extraJars/gsrs-spring-module-clinical-trials-3.2.6-SNAPSHOT-pom.xml -DpomFile=extraJars/gsrs-spring-module-clinical-trials-3.2.6-SNAPSHOT-pom.xml
+./mvnw install:install-file -Dfile=extraJars/gsrs-spring-module-drug-products-3.2.6-SNAPSHOT-pom.xml -DpomFile=extraJars/gsrs-spring-module-drug-products-3.2.6-SNAPSHOT-pom.xml
 
 ./mvnw install:install-file -Dfile=extraJars/applications-api-3.2.6-SNAPSHOT.jar 
 ./mvnw install:install-file -Dfile=extraJars/products-api-3.2.6-SNAPSHOT.jar
