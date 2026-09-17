402f2377822b14aa494b32094ed469079dd18da2
Mitch Miller <mitch.miller@thinkscience.us>
2026-09-17 15:16:33 -0400

    making another class work with autowiring

--- gsrs-module-substances-data-exchange/src/main/java/gsrs/dataexchange/autoconfigure/SubstanceDataExchangeAutoConfiguration.java
+++ gsrs-module-substances-data-exchange/src/main/java/gsrs/dataexchange/autoconfigure/SubstanceDataExchangeAutoConfiguration.java
@@ -2,6 +2,8 @@ package gsrs.dataexchange.autoconfigure;
 
 import gsrs.GsrsFactoryConfiguration;
 import gsrs.dataexchange.extractors.ExplicitMatchableExtractorFactory;
+import gsrs.module.substance.services.ConfigBasedDefinitionalElementFactory;
+import gsrs.module.substance.services.DefinitionalElementFactory;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
@@ -18,5 +20,11 @@ public class SubstanceDataExchangeAutoConfiguration {
                 new ExplicitMatchableExtractorFactory();
         factory.setGsrsFactoryConfiguration(configuration);
         return factory;
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public DefinitionalElementFactory definitionalElementFactory() {
+        return new ConfigBasedDefinitionalElementFactory();
     }
 }
